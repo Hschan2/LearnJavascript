8011723b7795400de043bd762a6ea5cf7ada987f
Hchan2 <seongchan_@naver.com>
2022-09-12 16:04:16 +0900

    알람 발생 시 Title에도 반응

--- About_Javascript/pushNotification.js
+++ About_Javascript/pushNotification.js
@@ -32,6 +32,7 @@ button.addEventListener("click", () => {
  */
 let notification
 let interval
+let alert = false
     
 document.addEventListener("visibilitychange", () => {
     if (document.visibilityState === "hidden") {
@@ -42,9 +43,13 @@ document.addEventListener("visibilitychange", () => {
                 body: `기존 탭으로 되돌아가주세요. ${Math.round((new Date() - leaveTime) / 1000)}초 경과`,
                 tag: "Please comeback previous tab",
             })
+
+            document.title = alert ? "푸시 알람" : "(1) New Message"
+            alert = !alert
         }, 100)
     } else {
         if (interval) clearInterval(interval)
         if (notification) notification.close();
+        if (alert) alert = false
     }
 })
