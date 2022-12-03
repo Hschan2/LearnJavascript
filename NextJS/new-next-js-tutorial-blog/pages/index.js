7cdd803e5b177259a3136b6726f9cb596a9d628d
Hchan2 <seongchan_@naver.com>
2022-12-03 21:54:14 +0900

    Routing

--- NextJS/new-next-js-tutorial-blog/pages/index.js
+++ NextJS/new-next-js-tutorial-blog/pages/index.js
@@ -7,12 +7,13 @@ export default function Home() {
     <div className="container">
       <h1 className='my-4'>환영합니다.</h1>
       <div className="Cards">
-        <div class="card" style={{width: '18rem'}}>
-          <img src="http://t1.daumcdn.net/brunch/service/user/8TE/image/2xXGrL1iffAGXuY3oPKrhmTqma4.png" class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h5 class="card-title">Next JS 튜토리얼</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="/blogpost/js-tutorial" class="btn btn-primary">Read More</a>
+        <div className="card" style={{width: '18rem'}}>
+          <img src="http://t1.daumcdn.net/brunch/service/user/8TE/image/2xXGrL1iffAGXuY3oPKrhmTqma4.png" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Next JS 튜토리얼</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            {/* blogpost 폴더의 [slug] 파일로 이동 */}
+            <a href="/blogpost/js-tutorial" className="btn btn-primary">Read More</a>
           </div>
         </div>
       </div>
