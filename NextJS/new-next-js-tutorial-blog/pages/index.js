32646809b81a6fbe20f2972ae48f3ed0fe10bce0
Hchan2 <seongchan_@naver.com>
2022-12-04 20:39:37 +0900

    개인 추가 작업, Fit images, substr 삼항연산자로 정리

--- NextJS/new-next-js-tutorial-blog/pages/index.js
+++ NextJS/new-next-js-tutorial-blog/pages/index.js
@@ -13,14 +13,18 @@ export default function Home({ blogs }) {
 
   return (
     <div className="container">
-      <h1 className='my-4 text-center'>환영합니다.</h1>
+      <h1 className='my-4 text-center'>환영합니다. Next JS 13</h1>
       <div className="Cards row justify-content-center">
         {blogs.blogs.map((blog) => {
-          return <div className="card mx-4 my-3" style={{width: '18rem'}}>
-            <img src="http://t1.daumcdn.net/brunch/service/user/8TE/image/2xXGrL1iffAGXuY3oPKrhmTqma4.png" className="card-img-top" alt="..." />
+          return <div className="card mx-4 my-3" style={{ width: '16rem', padding: '0' }}>
+            <img src="http://t1.daumcdn.net/brunch/service/user/8TE/image/2xXGrL1iffAGXuY3oPKrhmTqma4.png" className="card-img-top" alt="이미지" />
             <div className="card-body">
               <h5 className="card-title">{blog.title}</h5>
-              <p className="card-text">{blog.content.substr(0, 122)}...</p>
+              <p className="card-text">
+                {blog.content.length > 122
+                  ? `${blog.content.substr(0, 122)}...`
+                  : blog.content.substr(0, 122)}
+              </p>
               {/* blogpost 폴더의 [slug] 파일로 이동, Next JS 13 버전 Link안 a태그 삭제 */}
               <Link href={"blogpost/" + blog.slug} className="btn btn-primary">Read More</Link>
             </div>
