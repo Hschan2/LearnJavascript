3db056e2515c94d238e3c3a15c8729049c7bb421
Hchan2 <seongchan_@naver.com>
2022-12-04 19:30:38 +0900

    Next JS 13 버전, Link

--- NextJS/new-next-js-tutorial-blog/components/Navbar.js
+++ NextJS/new-next-js-tutorial-blog/components/Navbar.js
@@ -1,28 +1,32 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">Blog</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <Link href="/" className="navbar-brand">Blog</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/about">About Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/contact">Contact Us</a>
-                </li>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li className="nav-item">
+                          {/* Next JS 12버전 이하 Link */}
+                          {/* <Link href="/"><a className="nav-link active" aria-current="page">Home</a></Link> */}
+                          {/* Next JS 13버전 Link */}
+                          <Link href="/" className="nav-link active" aria-current="page">Home</Link>
+                      </li>
+                        <li className="nav-item">
+                            <Link href="/blogs" className="nav-link">About Us</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/contact" className="nav-link">Contact Us</Link>
+                        </li>
             </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button class="btn btn-outline-success" type="submit">Search</button>
+            <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
             </div>
         </div>
--- NextJS/new-next-js-tutorial-blog/pages/index.js
+++ NextJS/new-next-js-tutorial-blog/pages/index.js
@@ -1,22 +1,40 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import blogs from '../data/blogs.json'
+import { useEffect } from 'react'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Home({ blogs }) {
+  
+  useEffect(() => {
+    console.log(blogs);
+  }, [])
+
   return (
     <div className="container">
-      <h1 className='my-4'>환영합니다.</h1>
-      <div className="Cards">
-        <div className="card" style={{width: '18rem'}}>
-          <img src="http://t1.daumcdn.net/brunch/service/user/8TE/image/2xXGrL1iffAGXuY3oPKrhmTqma4.png" className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Next JS 튜토리얼</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            {/* blogpost 폴더의 [slug] 파일로 이동 */}
-            <a href="/blogpost/js-tutorial" className="btn btn-primary">Read More</a>
+      <h1 className='my-4 text-center'>환영합니다.</h1>
+      <div className="Cards row justify-content-center">
+        {blogs.blogs.map((blog) => {
+          return <div className="card mx-4 my-3" style={{width: '18rem'}}>
+            <img src="http://t1.daumcdn.net/brunch/service/user/8TE/image/2xXGrL1iffAGXuY3oPKrhmTqma4.png" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{blog.title}</h5>
+              <p className="card-text">{blog.content.substr(0, 122)}...</p>
+              {/* blogpost 폴더의 [slug] 파일로 이동, Next JS 13 버전 Link안 a태그 삭제 */}
+              <Link href={"blogpost/" + blog.slug} className="btn btn-primary">Read More</Link>
+            </div>
           </div>
-        </div>
+        })}
       </div>
     </div>
   )
+}
+
+// 서버사이드렌더링 기본 구조로 구현
+// Blogs.json 파일의 데이터를 Props로 이용
+export async function getServerSideProps(context) {
+  return {
+    props: {blogs},
+  }
 }
