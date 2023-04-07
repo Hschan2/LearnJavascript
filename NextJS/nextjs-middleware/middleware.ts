166657ab112aa4bf46b093794e93d2938817ae53
Hchan2 <seongchan_@naver.com>
2023-04-07 20:58:45 +0900

    Middleware 추가 기능

--- NextJS/nextjs-middleware/middleware.ts
+++ NextJS/nextjs-middleware/middleware.ts
@@ -1,4 +1,4 @@
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+import { NextFetchEvent, NextRequest, NextResponse, userAgent } from 'next/server'
 
 // Custom matcher config || Conditional statements
 const COOKIES = {
@@ -13,9 +13,24 @@ export const config = {
 }
 
 // Middleware 적용하기
-export function meddleware(req: NextRequest, ev: NextFetchEvent) {
+export function meddleware(req: NextRequest, res:NextResponse, ev: NextFetchEvent) {
     // Custom matcher config
     const token = req.cookies.get(COOKIES.JWT_TOKEN)
+    const url = req.nextUrl
+    // Check Web or Mobile
+    const { device } = userAgent(req)
+    const viewport = device.type === 'mobile' ? 'mobile' : 'desktop'
+    url.searchParams.set('viewport', viewport)
+
+    // Header
+    const requestHeaders = new Headers(req.headers)
+    requestHeaders.set('x-hello-from-middleware1', 'hello')
+
+    const responseHeader = NextResponse.next({
+        request: {
+            headers: requestHeaders,
+        }
+    })
 
     // /auth 하위에 모든 경로를 접속 시도할 때, cookie에 token이 존재하지 않으면 아래의 조건 실행
     if (!token) {
@@ -26,6 +41,29 @@ export function meddleware(req: NextRequest, ev: NextFetchEvent) {
     if (!token && req.nextUrl.pathname.includes('/auth')) {
 
     }
+
+    if (req.nextUrl.pathname.startsWith('/about')) {
+        return NextResponse.rewrite(new URL('/about2', req.url))
+    }
+
+    // Cookie
+    let cookie = req.cookies.get('nextjs')?.value
+    console.log(cookie) // 'fast
+    const allCookies = req.cookies.getAll()
+    console.log(allCookies) // [{ name: 'nextjs', value: 'fast' }]
+
+    req.cookies.has('nextjs') // true
+    req.cookies.delete('nextjs') // 삭제 -> has('nextjs') => false
+
+    const response = NextResponse.next() // ResponseCookies API로 응답에 대한 쿠키 설정
+    response.cookies.set('vercel', 'fast')
+    response.cookies.set({
+        name: 'vercel',
+        value: 'fast',
+        path: '/test',
+    })
+    cookie = response.cookies.get('vercel')
+    console.log(cookie) // => { name: 'vercel', value: 'fast', Path: '/test' }
 
     // 실제 사례
     if (!token && req.nextUrl.pathname.includes('/test')) {
