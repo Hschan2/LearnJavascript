7f7e73cad6177d2375829e43b60bb78071946f75
Hchan2 <seongchan_@naver.com>
2023-11-25 21:43:26 +0900

    스켈레톤 구현 실습

--- React/react-study/src/App.tsx
+++ React/react-study/src/App.tsx
@@ -1,9 +1,10 @@
-import { type } from 'os';
-import { useEffect, useState } from 'react';
-import './App.css'
+import { type } from "os";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Skeleton } from "./components/skeleton";
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const correctPassword = '4299';
+const correctPassword = "4299";
 
 type States = "typing" | "error" | "success";
 
@@ -11,6 +12,7 @@ function App() {
   // const [pressedNumbers, setPressedNumbers] = useState<number[]>([]);
   // const [error, setError] = useState("");
   // const [isCorrect, setIsCorrect] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   // useEffect를 사용하지 않아도 되지 않을까?
   // const [passcodeState, setPasscodeState] = useState<States>("typing");
@@ -21,14 +23,13 @@ function App() {
   if (pressedNumbers.length === correctPassword.length) {
     // 입력한 비밀번호와 정답 비밀번호가 같으면
     if (pressedNumbers === correctPassword) {
-      passcodeState = "success" // 입력 상태를 성공으로
+      passcodeState = "success"; // 입력 상태를 성공으로
     } else {
-      passcodeState = "error" // 입력 상태를 실패로
+      passcodeState = "error"; // 입력 상태를 실패로
     }
   }
 
   function handleNumberClicked(number: string) {
-
     /**
     const updatedPressNumbers = pressedNumbers + number;
     setPressedNumbers(updatedPressNumbers);
@@ -49,9 +50,9 @@ function App() {
 
     // 입력한 번호를 담는 변수의 길이가 정답 길이가 같으면 number를
     // 아니면 이전에 입력한 번호와 현재 입력한 번호값을 + (string이기 때문에 뒤에 저장)
-    setPressedNumbers((prev) => 
+    setPressedNumbers((prev) =>
       prev.length === correctPassword.length ? number : prev + number
-    )
+    );
   }
 
   /**
@@ -75,34 +76,47 @@ function App() {
   }, [pressedNumbers]);
   */
 
+  useEffect(() => {
+    // 데이터를 비동기적으로 가져온 후 데이터가 로드되었다고 가정
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <>
       {/* {JSON.stringify(pressedNumbers)} */}
 
       {/* isCorrect(패스워드 일치)일 경우, 'Success' 출력, 아니면 에러 메시지와 버튼 입력 폼 출력 */}
-      {passcodeState === 'success' ? (
+      {passcodeState === "success" ? (
         <div>Success!</div>
-      ): (
-          <div>
-            {passcodeState === 'error' && "bad password"}
-            <div className="number-pad">
-              {numbers.map((number, idx) => (
-                <button
-                  className={number === 0 ? 'zero' : ''}
-                  key={idx}
-                  onClick={() => {
-                    handleNumberClicked(number + "");
+      ) : (
+        <div>
+          {passcodeState === "error" && "bad password"}
+          <div className="number-pad">
+            {numbers.map((number, idx) => (
+              <button
+                className={number === 0 ? "zero" : ""}
+                key={idx}
+                onClick={() => {
+                  handleNumberClicked(number + "");
                   // setPressedNumbers(cur => [...cur, number]);
                 }}
-                >
-                  {number}
-                </button>
-              ))}
-            </div>
+              >
+                {number}
+              </button>
+            ))}
+          </div>
         </div>
       )}
+      {isLoading ? (
+        <Skeleton />
+      ) : (
+        // 실제 데이터 컴포넌트를 여기에 렌더링
+        <div>{/* 실제 데이터 */}</div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ React/react-study/src/components/Skeleton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Skeleton = () => {
+  return (
+    <div className="skeleton-loader">
+      <div className="skeleton-line"></div>
+      <div className="skeleton-line"></div>
+      <div className="skeleton-line"></div>
+    </div>
+  );
+};
