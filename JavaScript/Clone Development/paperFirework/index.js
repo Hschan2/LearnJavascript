53c4969c1c5cf065939273b346a62bab767865a0
Hchan2 <seongchan_@naver.com>
2023-11-25 21:43:53 +0900

    fix: 폭죽 애니메이션 크기 및 범위 수정

--- JavaScript/Clone Development/paperFirework/index.js
+++ JavaScript/Clone Development/paperFirework/index.js
@@ -11,27 +11,33 @@ class Particle {
         this.x = x;
         this.y = y;
         this.color = color;
-        this.size = 5;
-        this.velocityX = (Math.random() * 4 - 2) * 1.5;
-        this.velocityY = (-Math.random() * 4 + 2) * 1.5;
+        this.size = 6;
+        this.velocityX = Math.random() * 4 - 2;
+        this.velocityY = -Math.random() * 6 + 2;
         this.life = 150;
         this.rotation = Math.random() * 360;
-        this.accelerationX = Math.random() * 0.2 - 0.1;
-        this.accelerationY = Math.random() * 0.2 - 0.1;
+        this.shake = 0;
+        this.shakeSpeed = Math.random() * 0.2 + 0.2;
+        this.shakeDirection = Math.random() < 0.5 ? 0.5 : -0.5;
+        this.spinSpeed = Math.random() * 2 - 2;
     }
 
     update() {
         this.x += this.velocityX;
         this.y += this.velocityY;
-        this.velocityX += this.accelerationX;
-        this.velocityY += this.accelerationY;
+        this.velocityY += 0.1;
         this.life--;
-        this.rotation += Math.random() * 4 - 2;
+        this.rotation += this.spinSpeed * 2;
+        this.shake += this.shakeSpeed * this.shakeDirection;
+
+        if (Math.abs(this.shake) > 5) {
+            this.shakeDirection *= -1;
+        }
     }
 
     draw() {
         ctx.save();
-        ctx.translate(this.x, this.y);
+        ctx.translate(this.x + this.shake, this.y);
         ctx.rotate((this.rotation * Math.PI) / 180);
         ctx.fillStyle = this.color;
         ctx.fillRect(-this.size / 2, -this.size / 2, this.size, this.size);
@@ -52,8 +58,9 @@ canvas.addEventListener('click', (e) => {
 
 function createFirework(x, y) {
     const colors = ['#FF6961', '#FFB347', '#77DD77', '#6C8DFA', '#B4D9EF'];
+    const particleCount = 150;
 
-    for (let i = 0; i < 150; i++) {
+    for (let i = 0; i < particleCount; i++) {
         const color = colors[Math.floor(Math.random() * colors.length)];
         const particle = new Particle(x, y, color);
         particles.push(particle);
