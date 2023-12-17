b40cd839ee6dfef21922e9712bfbed2a2ea8694b
Hchan2 <seongchan_@naver.com>
2023-12-17 21:31:15 +0900

    Test: URL 링크로 동영상 출력 가능성 체크

--- Project/creator_portfolio/app/page.tsx
+++ Project/creator_portfolio/app/page.tsx
@@ -1,15 +1,10 @@
-import Image from 'next/image'
+import Video from "./components/video";
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <video
-        autoPlay={true}
-        muted={true}
-        loop={true}
-        style={{ width: "100%", height: "650px" }}
-        src={"https://www.youtube.com/watch?v=vVnzE904Pag"}
-      />
+      <Video />
     </main>
-  )
+  );
 }
