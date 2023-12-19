78547f0eb50d9696eeed5a0ce6e6ed6ceb13e7fa
Hchan2 <seongchan_@naver.com>
2023-12-19 22:39:49 +0900

    fix: 포트폴리오, 모션그래픽, Footer 컴포넌트 출력

--- Project/creator_portfolio/app/page.tsx
+++ Project/creator_portfolio/app/page.tsx
@@ -1,10 +1,17 @@
-import Video from "./components/video";
+import Title from "./components/Title";
+import Main from "./components/Main";
+import Motion from "./components/Motion";
+import Footer from "./components/Footer";
 
 export default function Home() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Video />
+    <main className="flex min-h-screen flex-col space-y-14 items-center justify-between p-10">
+      <Title />
+      <Main />
+      <Motion />
+      <Motion />
+      <Footer />
     </main>
   );
 }
