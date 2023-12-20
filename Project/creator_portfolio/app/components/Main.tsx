083de021abb152072726457a1d8c83efbc690ee2
Hchan2 <seongchan_@naver.com>
2023-12-20 22:30:20 +0900

    fix: 재생 아이콘 추가 및 마우스 접근 시 커서 모양 변경

--- Project/creator_portfolio/app/components/Main.tsx
+++ Project/creator_portfolio/app/components/Main.tsx
@@ -1,11 +1,14 @@
 import VIDEOS from "../constants/Videos";
 
 function Main() {
-
   return (
     <div>
-      <h1 className="text-xl font-sans font-bold mb-5">{VIDEOS.portfolio.title}</h1>
-      <div className="w-96 h-80 bg-gray-200 rounded-lg"></div>
+      <h1 className="text-xl font-sans font-bold mb-5">
+        {VIDEOS.title.portfolio}
+      </h1>
+      <div className="w-96 h-80 bg-gray-200 rounded-lg flex items-center justify-center text-7xl text-gray-500 hover:text-black cursor-pointer">
+        ▶️
+      </div>
     </div>
   );
 }
--- Project/creator_portfolio/app/components/Motion.tsx
+++ Project/creator_portfolio/app/components/Motion.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import VIDEOS from "../constants/Videos";
-import { Video } from "../type/VideoType";
+import { IVideo } from "../type/VideoType";
 
 function Motion() {
   return (
     <div>
-      <h1 className="text-xl font-sans font-bold mb-5">제목</h1>
+      <h1 className="text-xl font-sans font-bold mb-5">{VIDEOS.title.motionGraphic}</h1>
       <div className="grid grid-cols-3 gap-2">
-        {VIDEOS.motionGraphics.map((motion: Video) => (
+        {VIDEOS.motionGraphics.map((motion: IVideo) => (
           <div className="space-y-2 mb-2" key={motion.url}>
-            <p className="w-32 h-28 bg-gray-200 rounded-lg" />
+            <p className="w-32 h-28 bg-gray-200 rounded-lg flex items-center justify-center text-3xl text-gray-500 hover:text-black cursor-pointer">
+              ▶️
+            </p>
             <p className="text-xs">{motion.title}</p>
           </div>
         ))}
