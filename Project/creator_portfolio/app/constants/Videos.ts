0d605ede4dd89be7c6e45d4cffa8f3ba7b040789
Hchan2 <seongchan_@naver.com>
2023-12-20 22:29:54 +0900

    fix: 타입명 IVideo로 수정 (인터페이스임을 지칭)

--- Project/creator_portfolio/app/constants/Videos.ts
+++ Project/creator_portfolio/app/constants/Videos.ts
@@ -1,8 +1,11 @@
-import { Video } from "../type/VideoType";
+import { IVideo } from "../type/VideoType";
 
 const VIDEOS = {
+  title: {
+    portfolio: "포트폴리오",
+    motionGraphic: "모션그래픽",
+  },
   portfolio: {
-    title: "포트폴리오",
     url: "portfolio-url",
   },
   motionGraphics: [
@@ -10,7 +13,7 @@ const VIDEOS = {
     { title: "모션그래픽 두 번째", url: "motion-url-2" },
     { title: "모션그래픽 세 번째", url: "motion-url-3" },
     { title: "모션그래픽 네 번째", url: "motion-url-4" },
-  ] as Video[],
+  ] as IVideo[],
 };
 
 export default VIDEOS;
--- Project/creator_portfolio/app/type/VideoType.ts
+++ Project/creator_portfolio/app/type/VideoType.ts
@@ -1,4 +1,4 @@
-export interface Video {
+export interface IVideo {
   title: string;
   url: string;
 }
