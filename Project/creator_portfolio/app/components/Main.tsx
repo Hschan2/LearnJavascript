04bafd9f0fa121bb093e8823baa5e68a3acacf44
Hchan2 <seongchan_@naver.com>
2023-12-21 21:30:44 +0900

    fix: 각 프로젝트 제목 컴포넌트, 비디오 버튼 컴포넌트 사용

--- Project/creator_portfolio/app/components/Main.tsx
+++ Project/creator_portfolio/app/components/Main.tsx
@@ -1,14 +1,12 @@
 import VIDEOS from "../constants/Videos";
+import ProjectTitle from "../ui/ProjectTitle";
+import VideoButton from "../ui/VideoButton";
 
 function Main() {
   return (
     <div>
-      <h1 className="text-xl font-sans font-bold mb-5">
-        {VIDEOS.title.portfolio}
-      </h1>
-      <div className="w-96 h-80 bg-gray-200 rounded-lg flex items-center justify-center text-7xl text-gray-500 hover:text-black cursor-pointer">
-        ▶️
-      </div>
+      <ProjectTitle>{VIDEOS.title.portfolio}</ProjectTitle>
+      <VideoButton size="w-96 h-80 text-7xl" />
     </div>
   );
 }
--- Project/creator_portfolio/app/components/Motion.tsx
+++ Project/creator_portfolio/app/components/Motion.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import VIDEOS from "../constants/Videos";
 import { IVideo } from "../type/VideoType";
+import ProjectTitle from "../ui/ProjectTitle";
+import VideoButton from "../ui/VideoButton";
 
 function Motion() {
   return (
     <div>
-      <h1 className="text-xl font-sans font-bold mb-5">{VIDEOS.title.motionGraphic}</h1>
+      <ProjectTitle>{VIDEOS.title.motionGraphic}</ProjectTitle>
       <div className="grid grid-cols-3 gap-2">
         {VIDEOS.motionGraphics.map((motion: IVideo) => (
           <div className="space-y-2 mb-2" key={motion.url}>
-            <p className="w-32 h-28 bg-gray-200 rounded-lg flex items-center justify-center text-3xl text-gray-500 hover:text-black cursor-pointer">
-              ▶️
-            </p>
+            <VideoButton size="w-32 h-28 text-3xl" />
             <p className="text-xs">{motion.title}</p>
           </div>
         ))}
