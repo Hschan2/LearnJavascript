f8c39797c438646166f1a0ac31dc2f0a6748d54b
Hchan2 <seongchan_@naver.com>
2023-12-23 21:58:38 +0900

    fix: 모달창 닫을 때, 비디오 재생도 정지되도록 수정

--- Project/creator_portfolio/app/components/Modal.tsx
+++ Project/creator_portfolio/app/components/Modal.tsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef } from "react";
 import { IModalProps } from "../type/Types";
 
-function Modal({ isOpen, onClose, children }: IModalProps) {
+function Modal({ isOpen, onClose }: IModalProps) {
   const modalRef = useRef<HTMLDivElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
     const handleCancel = (e: MouseEvent) => {
       if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+        stopVideo();
         onClose();
       }
     };
@@ -20,20 +22,41 @@ function Modal({ isOpen, onClose, children }: IModalProps) {
     };
   }, [isOpen, onClose]);
 
+  const stopVideo = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+      videoRef.current.currentTime = 0;
+    }
+  };
+
   return (
     <div
       className={`fixed top-0 left-0 w-full h-full flex items-center justify-center ${
         isOpen ? "opacity-100 visible" : "opacity-0 invisible"
       } transition-opacity ease-in-out duration-300`}
     >
-      <div ref={modalRef} className="bg-white p-4 rounded-lg shadow-lg border relative w-2/3 h-96 max-w-3xl flex flex-col items-center justify-center">
+      <div
+        ref={modalRef}
+        className="bg-white rounded-lg shadow-lg border relative"
+      >
         <button
-          onClick={onClose}
-          className="absolute top-2 right-2 w-6 h-6 bg-transparent border rounded text-sm hover:border hover:border-gray-500 focus:outline-none"
+          onClick={() => {
+            stopVideo();
+            onClose();
+          }}
+          className="transition absolute top-2 right-2 w-6 h-6 bg-transparent text-sm text-slate-300 z-50 hover:text-white focus:outline-none"
         >
           X
         </button>
-        {children}
+        <video
+          ref={videoRef}
+          width="100%"
+          height="auto"
+          controls
+          autoPlay={true}
+        >
+          <source src="/videos/Cinderella.mp4" type="video/mp4" />
+        </video>
       </div>
     </div>
   );
