2d4f370b6769f3a5b349a188412be329e99c04b3
Hchan2 <seongchan_@naver.com>
2023-12-23 21:59:43 +0900

    fix: 테스트를 위해 여행 값 추가

--- Project/creator_portfolio/app/constants/Videos.ts
+++ Project/creator_portfolio/app/constants/Videos.ts
@@ -4,6 +4,7 @@ const VIDEOS = {
   title: {
     portfolio: "포트폴리오",
     motionGraphic: "모션그래픽",
+    travel: "여행",
   },
   portfolio: {
     url: "portfolio-url",
@@ -13,6 +14,12 @@ const VIDEOS = {
     { title: "모션그래픽 두 번째", url: "motion-url-2" },
     { title: "모션그래픽 세 번째", url: "motion-url-3" },
     { title: "모션그래픽 네 번째", url: "motion-url-4" },
+  ] as IVideo[],
+  travel: [
+    { title: "여행영상 첫 번째", url: "motion-url-1" },
+    { title: "여행영상 두 번째", url: "motion-url-2" },
+    { title: "여행영상 세 번째", url: "motion-url-3" },
+    { title: "여행영상 네 번째", url: "motion-url-4" },
   ] as IVideo[],
 };
 
