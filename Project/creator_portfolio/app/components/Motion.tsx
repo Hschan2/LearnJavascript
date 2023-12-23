9aa4c04b1038c448c38db4d6391b1546aea45889
Hchan2 <seongchan_@naver.com>
2023-12-23 21:59:07 +0900

    fix: pages에서 전달받은 카테고리 값들을 사용해 출력

--- Project/creator_portfolio/app/components/Motion.tsx
+++ Project/creator_portfolio/app/components/Motion.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import VIDEOS from "../constants/Videos";
-import { IVideo } from "../type/VideoType";
+import { IVideo, IVideoCategory } from "../type/VideoType";
 import ProjectTitle from "../ui/ProjectTitle";
 import VideoButton from "./VideoButton";
 
-function Motion() {
+function Motion({videoCategory, categoryTitle}: IVideoCategory) {
   return (
     <div>
-      <ProjectTitle>{VIDEOS.title.motionGraphic}</ProjectTitle>
+      <ProjectTitle>{categoryTitle}</ProjectTitle>
       <div className="grid grid-cols-3 gap-2">
-        {VIDEOS.motionGraphics.map((motion: IVideo) => (
+        {videoCategory.map((motion: IVideo) => (
           <div className="space-y-2 mb-2" key={motion.url}>
             <VideoButton size="w-32 h-28 text-3xl" />
             <p className="text-xs">{motion.title}</p>
