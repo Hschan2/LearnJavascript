1a457d2f369448f1288d7c85a2f6ef1155ee336a
Hchan2 <seongchan_@naver.com>
2023-12-23 21:59:49 +0900

    fix: 코드 줄 정리

--- Project/creator_portfolio/app/layout.tsx
+++ Project/creator_portfolio/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: '콘텐츠 모음',
-  description: '지금까지 제작한 영상들을 모아둔 장소입니다.',
-}
+  title: "콘텐츠 모음",
+  description: "지금까지 제작한 영상들을 모아둔 장소입니다.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
