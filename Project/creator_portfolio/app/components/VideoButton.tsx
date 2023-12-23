a3b4838ea73d353254146e0b0884ff69e10e0316
Hchan2 <seongchan_@naver.com>
2023-12-23 21:59:29 +0900

    fix: video 요소를 모달 컴포넌트로 이동

--- Project/creator_portfolio/app/components/VideoButton.tsx
+++ Project/creator_portfolio/app/components/VideoButton.tsx
@@ -19,15 +19,11 @@ function VideoButton({ size }: IButtonProps) {
     <div>
       <button
         onClick={openModal}
-        className={`${size} bg-gray-200 rounded-lg flex items-center justify-center text-gray-500 hover:text-black`}
+        className={`transition ${size} bg-gray-200 rounded-lg flex items-center justify-center text-gray-500 hover:text-black`}
       >
         ▶️
       </button>
-      <Modal isOpen={isModalOpen} onClose={closeModal}>
-        <video width="100%" height="auto" controls autoPlay>
-          <source src="/videos/Cinderella.mp4" type="video/mp4" />
-        </video>
-      </Modal>
+      <Modal isOpen={isModalOpen} onClose={closeModal} />
     </div>
   );
 }
