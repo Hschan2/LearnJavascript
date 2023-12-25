a4304db777dcda724a370e3eb9b7d74c87b05181
Hchan2 <seongchan_@naver.com>
2023-12-25 17:21:40 +0900

    fix: 코드 정리

--- Project/creator_portfolio/app/components/common/Modal.tsx
+++ Project/creator_portfolio/app/components/common/Modal.tsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useRef } from "react";
 import { IModalProps } from "../../type/Types";
+import VideoPlayer from "./VideoPlayer";
 
 function Modal({ isOpen, onClose }: IModalProps) {
   const modalRef = useRef<HTMLDivElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  useEffect(() => {
-    const handleCancel = (e: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
-        stopVideo();
-        onClose();
-      }
-    };
+  const handleCancel = (e: MouseEvent) => {
+    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+      allClose();
+    }
+  };
 
+  const stopVideo = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+      videoRef.current.currentTime = 0;
+    }
+  };
+
+  const allClose = () => {
+    stopVideo();
+    onClose();
+  };
+
+  useEffect(() => {
     if (isOpen) {
       document.addEventListener("mousedown", handleCancel);
     }
@@ -22,11 +34,27 @@ function Modal({ isOpen, onClose }: IModalProps) {
     };
   }, [isOpen, onClose]);
 
-  const stopVideo = () => {
-    if (videoRef.current) {
-      videoRef.current.pause();
-      videoRef.current.currentTime = 0;
-    }
+  const renderModalContent = () => {
+    return (
+      <div
+        ref={modalRef}
+        className="bg-white rounded-lg shadow-lg border relative"
+      >
+        <button
+          onClick={allClose}
+          className="transition absolute top-2 right-2 w-6 h-6 bg-transparent text-sm text-slate-300 z-50 hover:text-white focus:outline-none"
+        >
+          X
+        </button>
+        {isOpen && (
+          <VideoPlayer
+            videoLink="/videos/Cinderella.mp4"
+            videoRef={videoRef}
+            isOpen={isOpen}
+          />
+        )}
+      </div>
+    );
   };
 
   return (
@@ -35,29 +63,7 @@ function Modal({ isOpen, onClose }: IModalProps) {
         isOpen ? "opacity-100 visible" : "opacity-0 invisible"
       } transition-opacity ease-in-out duration-300`}
     >
-      <div
-        ref={modalRef}
-        className="bg-white rounded-lg shadow-lg border relative"
-      >
-        <button
-          onClick={() => {
-            stopVideo();
-            onClose();
-          }}
-          className="transition absolute top-2 right-2 w-6 h-6 bg-transparent text-sm text-slate-300 z-50 hover:text-white focus:outline-none"
-        >
-          X
-        </button>
-        <video
-          ref={videoRef}
-          width="100%"
-          height="auto"
-          controls
-          autoPlay={true}
-        >
-          <source src="/videos/Cinderella.mp4" type="video/mp4" />
-        </video>
-      </div>
+      {renderModalContent()}
     </div>
   );
 }
