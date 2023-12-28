54ce44b21b24bb093bd681cd0a326aa64ecb13d0
Hchan2 <seongchan_@naver.com>
2023-12-28 22:20:27 +0900

    fix: 동영상, 이미지 가져오기(테스트를 위한)

--- Project/creator_portfolio/app/constants/Videos.ts
+++ Project/creator_portfolio/app/constants/Videos.ts
@@ -7,19 +7,52 @@ const VIDEOS = {
     travel: "여행",
   },
   portfolio: {
-    url: "portfolio-url",
+    url: "/videos/Cinderella.mp4",
+    image: "/images/paper-Cut-winter.png",
   },
   motionGraphics: [
-    { title: "모션그래픽 첫 번째", url: "motion-url-1" },
-    { title: "모션그래픽 두 번째", url: "motion-url-2" },
-    { title: "모션그래픽 세 번째", url: "motion-url-3" },
-    { title: "모션그래픽 네 번째", url: "motion-url-4" },
+    {
+      title: "모션그래픽 첫 번째",
+      url: "/videos/Cinderella.mp4",
+      image: "/images/paper-Cut-winter.png",
+    },
+    {
+      title: "모션그래픽 두 번째",
+      url: "/videos/Cinderella.mp4",
+      image: "/images/paper-Cut-winter.png",
+    },
+    {
+      title: "모션그래픽 세 번째",
+      url: "/videos/Cinderella.mp4",
+      image: "/images/paper-Cut-winter.png",
+    },
+    {
+      title: "모션그래픽 네 번째",
+      url: "/videos/Cinderella.mp4",
+      image: "/images/paper-Cut-winter.png",
+    },
   ] as IVideo[],
   travel: [
-    { title: "여행영상 첫 번째", url: "motion-url-1" },
-    { title: "여행영상 두 번째", url: "motion-url-2" },
-    { title: "여행영상 세 번째", url: "motion-url-3" },
-    { title: "여행영상 네 번째", url: "motion-url-4" },
+    {
+      title: "여행영상 첫 번째",
+      url: "/videos/Cinderella.mp4",
+      image: "/images/paper-Cut-winter.png",
+    },
+    {
+      title: "여행영상 두 번째",
+      url: "/videos/Cinderella.mp4",
+      image: "/images/paper-Cut-winter.png",
+    },
+    {
+      title: "여행영상 세 번째",
+      url: "/videos/Cinderella.mp4",
+      image: "/images/paper-Cut-winter.png",
+    },
+    {
+      title: "여행영상 네 번째",
+      url: "/videos/Cinderella.mp4",
+      image: "/images/paper-Cut-winter.png",
+    },
   ] as IVideo[],
 };
 
