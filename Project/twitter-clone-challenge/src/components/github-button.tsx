e6d139be5fd6a27569b440fcf051cbdd263a3a57
Hchan2 <seongchan_@naver.com>
2024-01-16 21:56:08 +0900

    feat: 깃허브 로그인 컴포넌트 생성

---
+++ Project/twitter-clone-challenge/src/components/github-button.tsx
@@ -0,0 +1,46 @@
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import React from "react";
+import { styled } from "styled-components";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router";
+
+const Button = styled.span`
+  margin-top: 50px;
+  background-color: white;
+  font-weight: 600;
+  width: 100%;
+  color: black;
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: 0;
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+const Logo = styled.img`
+  height: 25px;
+`;
+
+function GithubButton() {
+  const navigate = useNavigate();
+  const onClick = async () => {
+    try {
+      const provider = new GithubAuthProvider();
+      await signInWithPopup(auth, provider);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <Button onClick={onClick}>
+      <Logo src="github-logo.svg" />
+      Github로 로그인
+    </Button>
+  );
+}
+
+export default GithubButton;
