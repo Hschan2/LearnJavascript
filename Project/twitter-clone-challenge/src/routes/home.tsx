65909b9d14a3ec1198a1e2ead5b632b91e823b23
Hchan2 <seongchan_@naver.com>
2024-01-16 21:55:22 +0900

    fix: 테스트를 위해 로그아웃 구현

--- Project/twitter-clone-challenge/src/routes/home.tsx
+++ Project/twitter-clone-challenge/src/routes/home.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import { auth } from "../firebase";
 
 function Home() {
+  const logOut = () => {
+    auth.signOut();
+  };
   return (
-    <div>home</div>
-  )
+    <h1>
+      <button onClick={logOut}>Log Out</button>
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
