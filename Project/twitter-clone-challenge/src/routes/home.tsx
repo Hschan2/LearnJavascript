c2f035390016cffd436f35a543fc5d96d581cae6
Hchan2 <seongchan_@naver.com>
2024-01-17 22:35:49 +0900

    fix: 메뉴 바 추가

--- Project/twitter-clone-challenge/src/routes/home.tsx
+++ Project/twitter-clone-challenge/src/routes/home.tsx
@@ -1,14 +1,13 @@
-import React from "react";
-import { auth } from "../firebase";
+import { styled } from "styled-components";
+import PostTweetForm from "../components/post-tweet-form";
+
+const Wrapper = styled.div``;
 
 function Home() {
-  const logOut = () => {
-    auth.signOut();
-  };
   return (
-    <h1>
-      <button onClick={logOut}>Log Out</button>
-    </h1>
+    <Wrapper>
+      <PostTweetForm />
+    </Wrapper>
   );
 }
 
