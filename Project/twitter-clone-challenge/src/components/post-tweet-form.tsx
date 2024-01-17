351675146aa76fa24d137015e0e1f568ec1ea823
Hchan2 <seongchan_@naver.com>
2024-01-17 22:36:00 +0900

    fix: 게시물 작성 1단계

---
+++ Project/twitter-clone-challenge/src/components/post-tweet-form.tsx
@@ -0,0 +1,118 @@
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
+import { styled } from "styled-components";
+import { auth, dateBase } from "../firebase";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
+const AttachFileButton = styled.label`
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const AttachFileInput = styled.input`
+  display: none;
+`;
+
+const SubmitButton = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.9;
+  }
+`;
+
+function PostTweetForm() {
+  const [isLoading, setLoading] = useState(false);
+  const [tweet, setTweet] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const onTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+  };
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    if (files && files.length === 1) {
+      setFile(files[0]);
+    }
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const user = auth.currentUser;
+      if (!user || isLoading || tweet === "" || tweet.length > 180) return;
+      try {
+          setLoading(true);
+          await addDoc(collection(dateBase, "tweets"), {
+              tweet,
+              createAt: Date.now(),
+              username: user.displayName || "익명",
+              userId: user.uid,
+          })
+      } catch (error) {
+          console.log(error);
+      } finally {
+          setLoading(false);
+      }
+  };
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <TextArea
+        rows={5}
+        maxLength={180}
+        onChange={onTextChange}
+        placeholder="당신의 이야기를 전달해 주세요."
+      />
+      <AttachFileButton htmlFor="file">
+        {file ? "사진 추가완료✔️" : "사진 추가"}
+      </AttachFileButton>
+      <AttachFileInput
+        onChange={onFileChange}
+        type="file"
+        id="file"
+        accept="image/*"
+      />
+      <SubmitButton
+        type="submit"
+        value={isLoading ? "보내는 중..." : "보내기"}
+      />
+    </Form>
+  );
+}
+
+export default PostTweetForm;
