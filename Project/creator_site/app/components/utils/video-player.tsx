8ffbf5515587c3642f7cc3c4a0e00064567bebe1
Hchan2 <seongchan_@naver.com>
2024-01-17 16:45:39 +0900

    feat: 모달창에 뜰 비디오 플레이어 컴포넌트

---
+++ Project/creator_site/app/components/utils/video-player.tsx
@@ -0,0 +1,21 @@
+import { IVideoPlayer } from "@/app/types/videoType";
+import React, { useEffect } from "react";
+
+function VideoPlayer({ videoLink, videoRef, isOpen }: IVideoPlayer) {
+  useEffect(() => {
+    if (isOpen && videoRef.current) {
+      videoRef.current.play().catch((error) => {
+        console.error("자동실행 오류:", error);
+      });
+    }
+  }, [isOpen, videoRef]);
+
+  return (
+    <video ref={videoRef} width="100%" height="auto" controls autoPlay={isOpen}>
+      <source src={videoLink} type="video/webm" />
+      <source src={videoLink} type="video/mp4" />
+    </video>
+  );
+}
+
+export default VideoPlayer;
