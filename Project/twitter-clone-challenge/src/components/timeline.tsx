9b557e743149d642f5ad6110af8a3cb2fcc01e8a
Hchan2 <seongchan_@naver.com>
2024-01-18 22:45:22 +0900

    feat: 트윗 실시간 업데이트

---
+++ Project/twitter-clone-challenge/src/components/timeline.tsx
@@ -0,0 +1,76 @@
+import { collection, limit, onSnapshot, orderBy, query } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { dateBase } from "../firebase";
+import { styled } from "styled-components";
+import Tweet from "./tweet";
+import { Unsubscribe } from "firebase/auth";
+
+export interface ITweet {
+  id: string;
+  photo?: string;
+  tweet: string;
+  userId: string;
+  username: string;
+  createdAt: number;
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  // overflow-y: scroll;
+`;
+
+function Timeline() {
+  const [tweets, setTweets] = useState<ITweet[]>([]);
+  useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(
+        collection(dateBase, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+      /* const spanShot = await getDocs(tweetsQuery);
+        const tweets = spanShot.docs.map((doc) => {
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        }); */
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, userId, username, photo } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            userId,
+            username,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweets(tweets);
+      });
+    };
+    fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
+  }, []);
+
+  return (
+    <Wrapper>
+      {tweets.map((tweet) => (
+        <Tweet key={tweet.id} {...tweet} />
+      ))}
+    </Wrapper>
+  );
+}
+
+export default Timeline;
