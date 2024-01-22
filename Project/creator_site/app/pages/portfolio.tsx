c287669930798a3029707402bbfb168cad5ffd31
Hchan2 <seongchan_@naver.com>
2024-01-22 16:23:33 +0900

    fix: VideoButton에 넘기는 Props 방식 변경

--- Project/creator_site/app/pages/portfolio.tsx
+++ Project/creator_site/app/pages/portfolio.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CATEGORIES_TITLE } from "../common/utils/constants";
+import { CATEGORIES_TITLE, PORTFOLIO_VIDEO } from "../common/utils/constants";
 import VideosTitle from "../components/utils/videos-title";
 import dynamic from "next/dynamic";
 
@@ -9,7 +9,10 @@ function Portfolio() {
   return (
     <div>
       <VideosTitle>{CATEGORIES_TITLE.portfolio}</VideosTitle>
-      <VideoButton size="w-96 h-80 text-7xl" />
+      <VideoButton
+        size="w-96 h-80"
+        data={PORTFOLIO_VIDEO}
+      />
     </div>
   );
 }
--- Project/creator_site/app/pages/videos.tsx
+++ Project/creator_site/app/pages/videos.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { ICategoriesProps, IVideo } from "../types/videoType";
 import VideosTitle from "../components/utils/videos-title";
+import VideoButton from "../components/utils/video-button";
 
-function Videos({categories}: ICategoriesProps) {
+function Videos({ categories, title }: ICategoriesProps) {
   return (
     <div>
-      <VideosTitle>제목</VideosTitle>
-      <div className="grid grid-cols-3 gap-2">
+      <VideosTitle>{title}</VideosTitle>
+      <div className="grid grid-cols-3 gap-3">
         {categories.map((category: IVideo) => (
-          <div className="space-y-2 mb-2" key={category.url}>
-            <p className="w-32 h-28 bg-gray-200 rounded-lg" />
-            <p className="text-xs">{category.title}</p>
-          </div>
+          <VideoButton
+            key={category.url}
+            size="w-32 h-28"
+            data={category}
+          />
         ))}
       </div>
     </div>
