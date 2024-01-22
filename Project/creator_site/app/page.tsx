e140e2ef031083651a4075b83f45bbac4dd244f6
Hchan2 <seongchan_@naver.com>
2024-01-22 16:24:05 +0900

    fix: 각 카테고리 별 내용 출력

--- Project/creator_site/app/page.tsx
+++ Project/creator_site/app/page.tsx
@@ -1,5 +1,5 @@
 import dynamic from "next/dynamic";
-import { CATEGORIES_VIDEOS } from "./common/utils/constants";
+import { CATEGORIES_TITLE, CATEGORIES_VIDEOS } from "./common/utils/constants";
 import Footer from "./pages/footer";
 import Title from "./pages/title";
 
@@ -8,10 +8,12 @@ const Videos = dynamic(() => import("./pages/videos"));
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 gap-y-16">
       <Title />
       <Portfolio />
-      <Videos categories={CATEGORIES_VIDEOS} />
+      <Videos categories={CATEGORIES_VIDEOS.motionGraphics} title={CATEGORIES_TITLE.motionGraphic} />
+      <Videos categories={CATEGORIES_VIDEOS.shortForm} title={CATEGORIES_TITLE.shortForm} />
+      <Videos categories={CATEGORIES_VIDEOS.travel} title={CATEGORIES_TITLE.travel} />
       <Footer />
     </main>
   );
