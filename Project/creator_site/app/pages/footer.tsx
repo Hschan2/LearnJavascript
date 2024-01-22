35107ac2f613fd06b9aa8edba53f1835b235a139
Hchan2 <seongchan_@naver.com>
2024-01-22 16:22:11 +0900

    fix: 색상 변경

--- Project/creator_site/app/pages/footer.tsx
+++ Project/creator_site/app/pages/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="text-xs text-gray-400 border-t-slate-400">
+    <div className="text-xs text-gray-300 border-t-slate-400">
       Copyrightⓒ 2023 All rights reserved by 홍성찬
     </div>
   );
