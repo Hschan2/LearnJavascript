a2436bdd9d5f986f3aae6052f1997e36e0d22b66
Hchan2 <seongchan_@naver.com>
2024-01-26 22:39:43 +0900

    fix: 수정 및 삭제 버튼 개선

--- Project/twitter-clone-challenge/src/components/tweet.tsx
+++ Project/twitter-clone-challenge/src/components/tweet.tsx
@@ -3,17 +3,17 @@ import { auth, dateBase, storage } from "../firebase";
 import { ITweet } from "./timeline";
 import { styled } from "styled-components";
 import { deleteDoc, doc } from "firebase/firestore";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import UpdateTweetForm from "./update-tweet-form";
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 3fr; /* 순서 변경 및 컬럼 비율 조정 */
+  grid-template-columns: 1fr 3fr;
   padding: 20px;
   border: 1px solid rgba(255, 255, 255, 0.5);
   border-radius: 15px;
   background-color: #111111;
-  position: relative; /* 상대 위치 지정 */
+  position: relative;
 `;
 
 const InfoContents = styled.div`
@@ -21,32 +21,22 @@ const InfoContents = styled.div`
   flex-direction: column;
 `;
 
-const MenuButton = styled.button`
+const Menu = styled.div`
   position: absolute;
   top: 10px;
   right: 10px;
-  width: 30px;
-  height: 30px;
-  background-color: #111111;
-  color: white;
-  border: 0;
-  cursor: pointer;
-`;
-
-const Menu = styled.div<{ visible: boolean }>`
-  position: absolute;
-  top: 40px;
-  right: 10px;
-  display: ${(props) => (props.visible ? "block" : "none")};
 `;
 
 const MenuItem = styled.button`
-  background-color: #111111;
-  color: white;
-  border: 1px solid rgba(255, 255, 255, 0.1);
+  color: #fff;
+  background-color: transparent;
+  border: none;
   cursor: pointer;
-  padding: 7px;
-  margin: 5px 0;
+  padding: 6px;
+  svg {
+    width: 18px;
+    height: 18px;
+  }
 `;
 
 const Photo = styled.img`
@@ -74,9 +64,7 @@ const Payload = styled.p`
 `;
 
 function Tweet({ username, photo, tweet, userId, id }: ITweet) {
-  const [menuVisible, setMenuVisible] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
   const user = auth.currentUser;
 
   const onDelete = async () => {
@@ -94,26 +82,6 @@ function Tweet({ username, photo, tweet, userId, id }: ITweet) {
       //
     }
   };
-
-  const handleMenuToggle = () => {
-    setMenuVisible(!menuVisible);
-  };
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (menuRef.current?.contains(event.target as Node)) {
-        return;
-      }
-
-      setMenuVisible(false);
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [menuRef]);
 
   return (
     <>
@@ -144,30 +112,39 @@ function Tweet({ username, photo, tweet, userId, id }: ITweet) {
           </InfoContents>
           {user?.uid === userId && (
             <>
-              <MenuButton onClick={handleMenuToggle}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                  />
-                </svg>
-              </MenuButton>
-              <Menu ref={menuRef} visible={menuVisible}>
-                <MenuItem onClick={() => setIsEdit(!isEdit)}>수정</MenuItem>
-                <MenuItem onClick={onDelete}>삭제</MenuItem>
+              <Menu>
+                <MenuItem onClick={() => setIsEdit(!isEdit)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                    />
+                  </svg>
+                </MenuItem>
+                <MenuItem onClick={onDelete}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </MenuItem>
               </Menu>
             </>
           )}
