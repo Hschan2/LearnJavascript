29e5e75b29493d960317c2e3863a58d466fd377d
Hchan2 <seongchan_@naver.com>
2024-01-28 22:03:54 +0900

    fix: 줄 정리

--- Project/twitter-clone-challenge/src/constants.ts
+++ Project/twitter-clone-challenge/src/constants.ts
@@ -1,3 +1,4 @@
 export const emailRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-export const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
+export const passwordRegex =
+  /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
