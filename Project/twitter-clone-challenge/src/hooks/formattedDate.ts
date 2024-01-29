a112fee7eb5dedbe2c699cf63dd9b5855666f436
Hchan2 <seongchan_@naver.com>
2024-01-29 22:20:01 +0900

    fix: 날짜 시간으로 나타내도록 수정

--- Project/twitter-clone-challenge/src/hooks/formattedDate.ts
+++ Project/twitter-clone-challenge/src/hooks/formattedDate.ts
@@ -3,14 +3,19 @@ interface createAtProps {
 }
 
 function formattedDate({ createdAt }: createAtProps) {
-  const createdDate = new Date(createdAt)
-    .toLocaleDateString("ko-KR", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-    })
-    .split(".");
-  return `${createdDate[0]}년 ${createdDate[1]}월 ${createdDate[2]}일`;
+  const createdDate = new Date(createdAt).toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  return createdDate.replace(
+    /(\d{4})\.(\d{2})\.(\d{2}) (\d{2}:\d{2}:\d{2})/,
+    "$1년 $2월 $3일 $4"
+  );
 }
 
 export default formattedDate;
