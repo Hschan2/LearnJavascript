ae39f561829d9942e60a811bcc477b618dcdc718
Hchan2 <seongchan_@naver.com>
2024-02-12 21:56:36 +0900

    feat: 좋아요 순 Tweet 나타내기

---
+++ Project/twitter-clone-challenge/src/routes/hot.tsx
@@ -0,0 +1,12 @@
+import Timeline from "../components/screen/timeline";
+import { Wrapper } from "./style/home-components";
+
+function Hot() {
+  return (
+    <Wrapper>
+      <Timeline isHot />
+    </Wrapper>
+  );
+}
+
+export default Hot;
