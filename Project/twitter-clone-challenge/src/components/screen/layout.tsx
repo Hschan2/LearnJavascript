3e623865df3b1fb597c36cc89ce58808fc7b54ac
Hchan2 <seongchan_@naver.com>
2024-02-12 21:56:59 +0900

    fix: Hot 페이지 추가

--- Project/twitter-clone-challenge/src/components/screen/layout.tsx
+++ Project/twitter-clone-challenge/src/components/screen/layout.tsx
@@ -1,5 +1,12 @@
 import { Outlet, useLocation } from "react-router";
-import { Logo, Menu, MenuItem, MenuName, NoneLineLink, Wrapper } from "../style/screen-components";
+import {
+  Logo,
+  Menu,
+  MenuItem,
+  MenuName,
+  NoneLineLink,
+  Wrapper,
+} from "../style/screen-components";
 
 function Layout() {
   const location = useLocation();
@@ -36,6 +43,23 @@ function Layout() {
               <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
             </svg>
             <MenuName>Home</MenuName>
+          </MenuItem>
+        </NoneLineLink>
+        <NoneLineLink to="/hot">
+          <MenuItem active={location.pathname === "/hot"}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12.963 2.286a.75.75 0 0 0-1.071-.136 9.742 9.742 0 0 0-3.539 6.176 7.547 7.547 0 0 1-1.705-1.715.75.75 0 0 0-1.152-.082A9 9 0 1 0 15.68 4.534a7.46 7.46 0 0 1-2.717-2.248ZM15.75 14.25a3.75 3.75 0 1 1-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 0 1 1.925-3.546 3.75 3.75 0 0 1 3.255 3.718Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <MenuName>Hot</MenuName>
           </MenuItem>
         </NoneLineLink>
         <NoneLineLink to="/profile">
