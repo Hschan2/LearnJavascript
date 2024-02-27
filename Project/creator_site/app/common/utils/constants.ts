c415788c1492061685b1c8b3e7ab66ce6b4956c3
Hchan2 <seongchan_@naver.com>
2024-02-27 22:07:43 +0900

    feat: 소셜 링크 값 추가

--- Project/creator_site/app/common/utils/constants.ts
+++ Project/creator_site/app/common/utils/constants.ts
@@ -10,209 +10,269 @@ export const CATEGORIES_TITLE = {
 
 export const PORTFOLIO_VIDEO = {
   url: "https://youtu.be/U99Ng5P4p94",
-  image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcodbKY%2FbtsES8Zs4uK%2FMbhXqykupwVANhEar2w59k%2Fimg.png",
+  image:
+    "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcodbKY%2FbtsES8Zs4uK%2FMbhXqykupwVANhEar2w59k%2Fimg.png",
 };
 
 export const CATEGORIES_VIDEOS = {
   motionGraphics: [
     {
+      title: "Rain on Street",
+      url: "https://youtu.be/eyXFrDSj39c",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FBEu1G%2FbtsFiM9JAJI%2FKrimurSPQgPxn6QUZKKmTK%2Fimg.png",
+    },
+    {
       title: "Among-Us Man",
       url: "https://youtu.be/aF6RaEgclQs",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbhjODU%2FbtsE2C6LhV0%2FsYnybQQZPS55akBa0DQAtk%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbhjODU%2FbtsE2C6LhV0%2FsYnybQQZPS55akBa0DQAtk%2Fimg.png",
     },
     {
       title: "Ball Slant",
       url: "https://youtu.be/pw7BNB2Cd4w",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbdhYoI%2FbtsES4vZnZy%2FdTet2xfwuwjCAOE9T6Ih9K%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbdhYoI%2FbtsES4vZnZy%2FdTet2xfwuwjCAOE9T6Ih9K%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} 뽐뽐뽐`,
       url: "https://youtu.be/VIlERhtaIeM",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FC0wGo%2FbtsERlE11GK%2FtKZSzEo3QLc3nA4pnWwYf0%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FC0wGo%2FbtsERlE11GK%2FtKZSzEo3QLc3nA4pnWwYf0%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} BitHit`,
       url: "https://youtu.be/47El80nOOm0",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcIVmPi%2FbtsEWgPvgo9%2Fa5gje6yAqnPiPGKTAjTHak%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcIVmPi%2FbtsEWgPvgo9%2Fa5gje6yAqnPiPGKTAjTHak%2Fimg.png",
     },
     {
       title: "Cartoon Typography",
       url: "https://youtu.be/ppqX2n9uYy8",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FepMfky%2FbtsEQN9BOK0%2F3hQQxjPkVAMHK9ToKWaBVk%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FepMfky%2FbtsEQN9BOK0%2F3hQQxjPkVAMHK9ToKWaBVk%2Fimg.png",
     },
     {
       title: "City Animation",
       url: "https://youtu.be/Z9-ilrzAURo",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdjntIr%2FbtsES8rFQL9%2FsKtyB5NdhVlr1t2FSHzE31%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdjntIr%2FbtsES8rFQL9%2FsKtyB5NdhVlr1t2FSHzE31%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Dorama Korea`,
       url: "https://youtu.be/eVwL3rXArj8",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fcr9diD%2FbtsERk0pjzs%2FoPSCCxKpl5cRgkbKHrxjNK%2Fimg.png",
-    },
-    {
-      title: "Dr Strange Portal",
-      url: "https://youtu.be/JGR3PR9MzII",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcUR4Zf%2FbtsERke3hjb%2FNHGmbReuv9jLY9hxGTgFYk%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fcr9diD%2FbtsERk0pjzs%2FoPSCCxKpl5cRgkbKHrxjNK%2Fimg.png",
     },
     {
       title: "8 Bit Graphic",
       url: "https://youtu.be/CDDDZBukGbk",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb7qAX5%2FbtsEVJqTEXT%2FZJKtYn3U3h5sSKJ1eD9Vk0%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb7qAX5%2FbtsEVJqTEXT%2FZJKtYn3U3h5sSKJ1eD9Vk0%2Fimg.png",
     },
     {
       title: "Face Animation",
       url: "https://youtu.be/UrN3Z9hUkt0",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbYSezV%2FbtsEVhg52UN%2F99UsM8rifX0Dg8ROOtahCk%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbYSezV%2FbtsEVhg52UN%2F99UsM8rifX0Dg8ROOtahCk%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Fake Mans`,
       url: "https://youtu.be/5X3w3_QKWH4",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb2LUyz%2FbtsETXb6HWt%2Fe8mMDyKxVBKd0nKZSe6hZ0%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb2LUyz%2FbtsETXb6HWt%2Fe8mMDyKxVBKd0nKZSe6hZ0%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} fromis_9 ③`,
       url: "https://youtu.be/h5CBbWdRG_o",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbplNaF%2FbtsEVdlueTe%2FlqChHQW8Yorxt0VT5VSKR1%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbplNaF%2FbtsEVdlueTe%2FlqChHQW8Yorxt0VT5VSKR1%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} fromis_9 ②`,
       url: "https://youtu.be/esaLk979NRA",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb6Cnwa%2FbtsEVkx6kbA%2FHv2SiFfuy7ibe4ndITp4BK%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb6Cnwa%2FbtsEVkx6kbA%2FHv2SiFfuy7ibe4ndITp4BK%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} fromis_9 ①`,
       url: "https://youtu.be/_kDIgeRSX0I",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbMgmZm%2FbtsEVc1bgsc%2Fu5D92l2RF5grrRdI2251L1%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbMgmZm%2FbtsEVc1bgsc%2Fu5D92l2RF5grrRdI2251L1%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Havanana AD`,
       url: "https://youtu.be/tcpWp216Dz8",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcZxonw%2FbtsEQMQoEMN%2FZzKvErSTnaMvSkXtaajmvK%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcZxonw%2FbtsEQMQoEMN%2FZzKvErSTnaMvSkXtaajmvK%2Fimg.png",
     },
     {
       title: "Hub Animation",
       url: "https://youtu.be/sbBRK97SDeo",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FEFFNN%2FbtsESqe0Kuv%2FHgMtYqV0dEunzOJxdEg0wk%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FEFFNN%2FbtsESqe0Kuv%2FHgMtYqV0dEunzOJxdEg0wk%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Ice Cream`,
       url: "https://youtu.be/YmYWfxc9VKs",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FNRaPO%2FbtsESp8fHkU%2F7YtzHKjXKo8kwDVKVrg2sk%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FNRaPO%2FbtsESp8fHkU%2F7YtzHKjXKo8kwDVKVrg2sk%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} IZ*ONE ENOZI`,
       url: "https://youtu.be/82LQFTiUCDE",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FvZIgg%2FbtsERnbLA4E%2Ff6hfQy3utJsQJpz2rcRjuk%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FvZIgg%2FbtsERnbLA4E%2Ff6hfQy3utJsQJpz2rcRjuk%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} IZ*ONE Arcade`,
       url: "https://youtu.be/h9AKS4kDqYs",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FFYsA6%2FbtsEVjlFn2l%2F5haNK8LEsZAkgDkXsRCa2k%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FFYsA6%2FbtsEVjlFn2l%2F5haNK8LEsZAkgDkXsRCa2k%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Line Friends`,
       url: "https://youtu.be/pnVvCM1Xd-U",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdXSTtn%2FbtsEVMHS34V%2FFSr3AK4QGbDH1v3qRRvKk1%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdXSTtn%2FbtsEVMHS34V%2FFSr3AK4QGbDH1v3qRRvKk1%2Fimg.png",
     },
     {
       title: "Line MotionGraphic",
       url: "https://youtu.be/6nc9l8iOWy0",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F7OGXs%2FbtsERnv5ti6%2FFF5u9GzQvyp1XOEyBkBMhK%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F7OGXs%2FbtsERnv5ti6%2FFF5u9GzQvyp1XOEyBkBMhK%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Netflix`,
       url: "https://youtu.be/F30cYdGQ-aY",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FLiVDM%2FbtsEVOeBt5K%2FPWkLHVfKEcrCNnng5wFw41%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FLiVDM%2FbtsEVOeBt5K%2FPWkLHVfKEcrCNnng5wFw41%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Netflix`,
       url: "https://youtu.be/gVhQf--bQIQ",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbjWqVf%2FbtsES4JxiEZ%2FjB1NRBg0qxWJ1Vquxl1jN0%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbjWqVf%2FbtsES4JxiEZ%2FjB1NRBg0qxWJ1Vquxl1jN0%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Pink Diary`,
       url: "https://youtu.be/3cwopGjyn6c",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FvcUIb%2FbtsEVcNDhW1%2Fo9R3dHrjKvABJEyglmN3K0%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FvcUIb%2FbtsEVcNDhW1%2Fo9R3dHrjKvABJEyglmN3K0%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} RocketPunch`,
       url: "https://youtu.be/Eprjf6rVknc",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbh7Ovl%2FbtsES1F1wwz%2F2kUtBFzkXXEBv1Y1whZWaK%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbh7Ovl%2FbtsES1F1wwz%2F2kUtBFzkXXEBv1Y1whZWaK%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} NAVER Series`,
       url: "https://youtu.be/zNHYENh9-XI",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbO0xVy%2FbtsEWgopYzI%2F84EHxEdcMxysxj2D9Y8KkK%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbO0xVy%2FbtsEWgopYzI%2F84EHxEdcMxysxj2D9Y8KkK%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} SNOW`,
       url: "https://youtu.be/HoKYF9E11fg",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FKvhOg%2FbtsETSaLBQG%2FW7Akq3bJ8R33esmPfnkxe1%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FKvhOg%2FbtsETSaLBQG%2FW7Akq3bJ8R33esmPfnkxe1%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Squid Game`,
       url: "https://youtu.be/XgVA1JbCS-o",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbfnRxq%2FbtsETVehcay%2F3uxK4iKdrMUQG3NAkdnxa1%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbfnRxq%2FbtsETVehcay%2F3uxK4iKdrMUQG3NAkdnxa1%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} TvN`,
       url: "https://youtu.be/mx6x4YswVdg",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FGeLAx%2FbtsEVNz0z1H%2FkkiNXnB3JJd2XW75hrjB81%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FGeLAx%2FbtsEVNz0z1H%2FkkiNXnB3JJd2XW75hrjB81%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Valorant`,
       url: "https://youtu.be/XMyRi3uDj3k",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblkxzS%2FbtsEWgopYzU%2F3zX2jcPUBhikwAXVtRR6a0%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblkxzS%2FbtsEWgopYzU%2F3zX2jcPUBhikwAXVtRR6a0%2Fimg.png",
     },
     {
       title: `${COVER_MESSAGE} Woolim Ent`,
       url: "https://youtu.be/yHFP0FvxEHs",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbG2nlu%2FbtsETkegZGE%2FYI4UbElbFX3ag6LowNCfKk%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbG2nlu%2FbtsETkegZGE%2FYI4UbElbFX3ag6LowNCfKk%2Fimg.png",
     },
   ] as IVideo[],
   travel: [
     {
       title: "유럽",
       url: "https://youtu.be/WU4QpsGcNP8",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FctrcSs%2FbtsERwT8KIn%2FSkHEep2bE7nqtXcSxkVAX1%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FctrcSs%2FbtsERwT8KIn%2FSkHEep2bE7nqtXcSxkVAX1%2Fimg.png",
     },
     {
       title: "일본",
       url: "https://youtu.be/G2vFJloXtSo",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fk9iNo%2FbtsEWKiuQ5M%2FbYV23YLE4693JtST6Z2su0%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fk9iNo%2FbtsEWKiuQ5M%2FbYV23YLE4693JtST6Z2su0%2Fimg.png",
     },
     {
       title: "가오슝",
       url: "https://youtu.be/OxLCSJdj364",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FEeUPS%2FbtsES4isCAW%2FFyqplNPhkCE6aV9rhKt7x0%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FEeUPS%2FbtsES4isCAW%2FFyqplNPhkCE6aV9rhKt7x0%2Fimg.png",
     },
     {
       title: "라오스",
       url: "https://youtu.be/JIXvVaRAef8",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbq1Q8m%2FbtsES8FaUeH%2F1gQRNmnUL9Zai3ZzKP67V0%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbq1Q8m%2FbtsES8FaUeH%2F1gQRNmnUL9Zai3ZzKP67V0%2Fimg.png",
     },
   ] as IVideo[],
   shortForm: [
     {
       title: "강원도 원주",
       url: "https://youtube.com/shorts/bO6g9SFzh_0?feature=share",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdPcM08%2FbtsEWM1Gxwb%2FUWDMqvmLAgyzTOTahMadUK%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FdPcM08%2FbtsEWM1Gxwb%2FUWDMqvmLAgyzTOTahMadUK%2Fimg.png",
     },
     {
       title: "일본 비에이",
       url: "https://youtube.com/shorts/LjlYtmrsYrc?feature=share",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F6HbwZ%2FbtsESKLmBgp%2FmKbFkIKuDLQGrEoYeZIRv0%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F6HbwZ%2FbtsESKLmBgp%2FmKbFkIKuDLQGrEoYeZIRv0%2Fimg.png",
     },
     {
       title: "일본 홋카이도",
       url: "https://youtube.com/shorts/yBkNh1IPwIs?feature=share",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FxrP1B%2FbtsEVhVDKIP%2FDjYebqIwJlCHyGIDkTMwp1%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FxrP1B%2FbtsEVhVDKIP%2FDjYebqIwJlCHyGIDkTMwp1%2Fimg.png",
     },
     {
       title: "강원도 고석정",
       url: "https://youtube.com/shorts/1X89ufkfaiE?feature=share",
-      image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FwZ81d%2FbtsEVNGMMhw%2FIBApwQRSMbLAvSSkCUL570%2Fimg.png",
+      image:
+        "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FwZ81d%2FbtsEVNGMMhw%2FIBApwQRSMbLAvSSkCUL570%2Fimg.png",
     },
   ] as IVideo[],
 };
+
+export const SOCIAL_LINKS = {
+  social: [
+    {
+      link: "https://blog.naver.com/seongchan_",
+      image: "/naver.png",
+      description: "Naver Blog",
+    },
+    {
+      link: "https://www.instagram.com/h_s.__.b/",
+      image: "/instagram.svg",
+      description: "Instagram",
+    },
+    {
+      link: "https://hchan2.tistory.com/",
+      image: "/tistory.svg",
+      description: "Tistory",
+    }
+  ]
+}
