82d1e9f421bc37b3ab66a11d31b3826be66ee2fc
Hchan2 <seongchan_@naver.com>
2024-03-11 22:15:39 +0900

    fix: react-modal 사용을 위해 네이밍 수정 및 코드 개선

--- Project/creator_site/app/components/utils/modal-container.tsx
+++ Project/creator_site/app/components/utils/modal-container.tsx
@@ -1,15 +1,16 @@
 import { IModalProps } from "@/app/types/modalType";
 import React, { useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
+import { createPortal } from "react-dom";
 
-const VideoPlayer = dynamic(() => import("./video-player"));
+const VideoPlayer = dynamic(() => import("./video-player"), { ssr: false });
 
-function Modal({ isOpen, onClose, videoUrl }: IModalProps) {
+function ModalContainer({ isOpen, onClose, videoUrl }: IModalProps) {
   const modalRef = useRef<HTMLDivElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
 
   const handleCancel = (e: MouseEvent) => {
-    if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+    if (!modalRef.current?.contains(e.target as Node)) {
       closeVideo();
     }
   };
@@ -27,17 +28,12 @@ function Modal({ isOpen, onClose, videoUrl }: IModalProps) {
   };
 
   useEffect(() => {
-    if (isOpen) {
-      document.addEventListener("mousedown", handleCancel);
-      if (videoRef.current) {
-        videoRef.current.focus();
-      }
-    }
+    document.addEventListener("click", handleCancel);
 
     return () => {
-      document.removeEventListener("mousedown", handleCancel);
+      document.removeEventListener("click", handleCancel);
     };
-  }, [isOpen, onClose]);
+  }, [onClose, handleCancel]);
 
   const renderModalContent = () => {
     return (
@@ -47,7 +43,7 @@ function Modal({ isOpen, onClose, videoUrl }: IModalProps) {
       >
         <button
           onClick={closeVideo}
-          className="transition absolute top-2 right-2 w-6 h-6 bg-transparent text-sm text-slate-300 z-50 hover:text-white focus:outline-none"
+          className="transition absolute top-2 right-2 w-6 h-6 bg-transparent font-bold text-slate-300 z-50 hover:text-white focus:outline-none"
         >
           X
         </button>
@@ -63,14 +59,17 @@ function Modal({ isOpen, onClose, videoUrl }: IModalProps) {
   };
 
   return (
-    <div
-      className={`fixed top-0 left-0 w-full h-full flex items-center justify-center ${
-        isOpen ? "opacity-100 visible z-50" : "opacity-0 invisible"
-      } transition-opacity ease-in-out duration-300`}
-    >
-      {renderModalContent()}
-    </div>
+    <>
+      {createPortal(
+        <div
+          className={`fixed top-0 left-0 w-full h-full flex items-center justify-center transition-opacity ease-in-out duration-300`}
+        >
+          {renderModalContent()}
+        </div>,
+        document.body
+      )}
+    </>
   );
 }
 
-export default Modal;
+export default ModalContainer;
