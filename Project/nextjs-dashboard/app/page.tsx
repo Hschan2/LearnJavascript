f8769db4e7d0079999f30c45356440e99142ae96
Hchan2 <seongchan_@naver.com>
2024-06-24 17:40:53 +0900

    feat: dashboard 페이지 생성

--- Project/nextjs-dashboard/app/page.tsx
+++ Project/nextjs-dashboard/app/page.tsx
@@ -1,12 +1,14 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -24,8 +26,21 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className="flex flex-col items-center justify-center p-6 gap-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className='hidden md:block'
+            alt='Screenshots of the dashborad project showing desktop version'
+          />
+          <Link
+            href="/dashboard"
+            className="flex items-center justify-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors w-full hover:bg-blue-400 md:text-base"
+          >
+            <span>DashBoard</span> <ArrowRightIcon className="w-5 md:w-6" />
+          </Link>
         </div>
       </div>
     </main>
