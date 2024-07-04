7708e94f4d14a63b457e0593679a41534efcc8eb
Hchan2 <seongchan_@naver.com>
2024-07-04 22:31:55 +0900

    feat: 글 추가하는 플러스 버튼 추가

--- Project/twitter-clone-challenge/src/components/screen/layout.tsx
+++ Project/twitter-clone-challenge/src/components/screen/layout.tsx
@@ -53,10 +53,24 @@ function Layout() {
           </NoneLineLink>
         </CenterMenuItem>
         <RightMenuItem>
+          <NoneLineLink to="/write">
+            <MenuItem>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </MenuItem>
+          </NoneLineLink>
           <NoneLineLink to="/profile">
-            <MenuItem
-              active={location.pathname === "/profile" ? "true" : "false"}
-            >
+            <MenuItem>
               {avatar ? (
                 <Avatar src={avatar} alt="Profile" />
               ) : (
@@ -76,9 +90,7 @@ function Layout() {
             </MenuItem>
           </NoneLineLink>
           <NoneLineLink to="/settings">
-            <MenuItem
-              active={location.pathname === "/settings" ? "true" : "false"}
-            >
+            <MenuItem>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
