87497cdda5ffdec183fabb81565b3b0b4e8645e8
Hchan2 <seongchan_@naver.com>
2024-07-14 23:03:18 +0900

    fix: 태그 데이터 가져오기

--- Project/twitter-clone-challenge/src/components/screen/timeline.tsx
+++ Project/twitter-clone-challenge/src/components/screen/timeline.tsx
@@ -18,7 +18,7 @@ function Timeline({ isHot }: ITimeline) {
   const [tweets, setTweets] = useState<ITweet[]>([]);
 
   const mapTweetData = (doc: QueryDocumentSnapshot): ITweet => {
-    const { tweet, createdAt, userId, username, photo, likes, likedBy, exclamation } = doc.data();
+    const { tweet, createdAt, userId, username, photo, likes, likedBy, exclamation, tags } = doc.data();
     return {
       tweet,
       createdAt,
@@ -29,6 +29,7 @@ function Timeline({ isHot }: ITimeline) {
       likes,
       likedBy,
       exclamation,
+      tags
     };
   };
 
