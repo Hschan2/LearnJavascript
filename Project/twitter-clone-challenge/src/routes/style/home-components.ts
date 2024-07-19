ad857c835c605408b01f7338ef0dae96a7a7b772
Hchan2 <seongchan_@naver.com>
2024-07-19 22:35:41 +0900

    fix: 페이지 바깥으로 넘어가는 경우 개선을 위해 width 값 조절

--- Project/twitter-clone-challenge/src/routes/style/home-components.ts
+++ Project/twitter-clone-challenge/src/routes/style/home-components.ts
@@ -1,12 +1,12 @@
 import { styled } from "styled-components";
 
 export const Wrapper = styled.div`
+  width: 90%;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 30px;
   padding: 10px 0;
-  width: 100%;
   margin: 0 auto;
   padding: 20px;
 `;
