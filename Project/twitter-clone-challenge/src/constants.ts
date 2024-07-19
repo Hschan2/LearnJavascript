a59e1d0b10a60748349978d5377b155605ff119f
Hchan2 <seongchan_@naver.com>
2024-07-19 22:31:54 +0900

    add: 아이템 필터 값 생성

--- Project/twitter-clone-challenge/src/constants.ts
+++ Project/twitter-clone-challenge/src/constants.ts
@@ -2,3 +2,14 @@ export const emailRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 export const passwordRegex =
   /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
+
+export const SELECT_OPTION_VALUE = [
+  "아이폰",
+  "갤럭시",
+  "소니",
+  "캐논",
+  "니콘",
+  "후지",
+  "라이카",
+  "기타",
+];
