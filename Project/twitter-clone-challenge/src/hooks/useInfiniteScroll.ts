75c98301512215a39d7074278436c45d86b39eba
Hchan2 <seongchan_@naver.com>
2024-07-26 22:25:07 +0900

    feat: 무한스크롤 훅 생성

---
+++ Project/twitter-clone-challenge/src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+type FetchMoreDataFn = () => Promise<void>;
+
+function useInfiniteScroll(
+  fetchMoreData: FetchMoreDataFn
+): [boolean, (node: HTMLElement | null) => void] {
+  const [isFetching, setIsFetching] = useState(false);
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  const handleObserver = useCallback((entries: IntersectionObserverEntry[]) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setIsFetching(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isFetching) {
+      fetchMoreData().finally(() => setIsFetching(false));
+    }
+  }, [isFetching, fetchMoreData]);
+
+  const triggerRef = useCallback(
+    (node: HTMLElement | null) => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver(handleObserver, {
+        threshold: 0.8,
+      });
+      if (node) observer.current.observe(node);
+    },
+    [handleObserver]
+  );
+
+  return [isFetching, triggerRef];
+}
+
+export default useInfiniteScroll;
