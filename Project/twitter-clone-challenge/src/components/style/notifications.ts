5f47c846bebca045fbe1519388ea473291f7e545
Hchan2 <seongchan_@naver.com>
2024-08-11 22:34:36 +0900

    feat: 알람 관련 스타일 및 타입 생성

---
+++ Project/twitter-clone-challenge/src/components/style/notifications.ts
@@ -0,0 +1,47 @@
+import { styled } from "styled-components";
+
+export const NotificationsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 100%;
+  margin: 20px 10px;
+  padding: 0 14px;
+`;
+
+export const NotificationContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid ${(props) => props.theme.border};
+  border-radius: 12px;
+  padding: 16px 12px;
+`;
+
+export const NotificationMessage = styled.span`
+  width: 90%;
+  font-size: clamp(12px, 2vw, 16px);
+  color: ${(props) => props.theme.text};
+`;
+
+export const NotificationTimeAgo = styled.span`
+  font-size: 12px;
+  color: ${(props) => props.theme.lightText};
+  margin-left: 12px;
+`;
+
+export const NotificationButton = styled.button`
+  border: none;
+  background: none;
+  color: ${(props) => props.theme.text};
+  cursor: pointer;
+`;
+
+export const NoneNotificationWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ Project/twitter-clone-challenge/src/components/types/notifications.ts
@@ -0,0 +1,11 @@
+export interface NotificationType {
+  id: string;
+  recipientId: string;
+  tweetTitle: string;
+  tweetId: string;
+  senderId: string;
+  senderName: string;
+  createdAt: string;
+  type: "like";
+  isRead: boolean;
+}
