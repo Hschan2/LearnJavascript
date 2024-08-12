7532a0c88ef150eb64d87d3a114f3f59051bcb11
Hchan2 <seongchan_@naver.com>
2024-08-12 22:27:47 +0900

    feat: 알람 데이터의 id 값을 가지고 특정 알람 데이터 지우기 기능 추가

--- Project/twitter-clone-challenge/src/routes/notification.tsx
+++ Project/twitter-clone-challenge/src/routes/notification.tsx
@@ -3,6 +3,8 @@ import { NotificationType } from "../components/types/notifications";
 import { auth, dataBase } from "../firebase";
 import {
   collection,
+  deleteDoc,
+  doc,
   onSnapshot,
   orderBy,
   query,
@@ -17,14 +19,19 @@ import {
   NotificationsWrapper,
 } from "../components/style/notifications";
 import { TimeAgo } from "../hooks/useTimeAgo";
+import useNotificationStore from "../components/store/useNotification";
 
 function Notification() {
   const [notifications, setNotifications] = useState<NotificationType[]>([]);
+  const setNewNotification = useNotificationStore(
+    (state) => state.setNotification
+  );
   const user = auth.currentUser;
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!user) return;
+    setNewNotification(false);
 
     const notificationQuery = query(
       collection(dataBase, "notifications"),
@@ -41,10 +48,20 @@ function Notification() {
     });
 
     return () => unsubscribe();
-  }, [user]);
+  }, [user, setNewNotification]);
 
   const handleNotificationClick = (notification: NotificationType) => {
     navigate(`/tweets/${notification.tweetId}`);
+  };
+
+  const deleteNotification = async (notificationId: string) => {
+    try {
+      const notificationRef = doc(dataBase, "notifications", notificationId);
+      await deleteDoc(notificationRef);
+      console.log("알람 삭제 완료");
+    } catch (error) {
+      console.error(`알람 삭제 에러 발생: ${error}`);
+    }
   };
 
   return (
@@ -61,7 +78,24 @@ function Notification() {
               {TimeAgo(new Date(notification.createdAt))}
             </NotificationTimeAgo>
           </NotificationMessage>
-          <NotificationButton>읽음</NotificationButton>
+          <NotificationButton
+            onClick={() => deleteNotification(notification.id)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+              />
+            </svg>
+          </NotificationButton>
         </NotificationContainer>
       ))}
     </NotificationsWrapper>
