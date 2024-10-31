e66953abc15a88207687459441e5812b5eab7521
Hchan2 <seongchan_@naver.com>
2024-10-31 22:26:20 +0900

    update: 코드 리팩토링

--- Project/twitter-clone-challenge/src/hooks/useTimeAgo.ts
+++ Project/twitter-clone-challenge/src/hooks/useTimeAgo.ts
@@ -1,26 +1,26 @@
 export const TimeAgo = (date: Date): string => {
   const now = new Date();
   const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
-  const diffInMinutes = Math.floor(diffInSeconds / 60);
-  const diffInHours = Math.floor(diffInMinutes / 60);
-  const diffInDays = Math.floor(diffInHours / 24);
-  const diffInWeeks = Math.floor(diffInDays / 7);
-  const diffInMonths = Math.floor(diffInDays / 30);
-  const diffInYears = Math.floor(diffInDays / 365);
 
   if (diffInSeconds < 60) {
     return `${diffInSeconds}초 전`;
-  } else if (diffInMinutes < 60) {
+  } else if (diffInSeconds < 3600) {
+    const diffInMinutes = Math.floor(diffInSeconds / 60);
     return `${diffInMinutes}분 전`;
-  } else if (diffInHours < 24) {
+  } else if (diffInSeconds < 86400) {
+    const diffInHours = Math.floor(diffInSeconds / 3600);
     return `${diffInHours}시간 전`;
-  } else if (diffInDays < 7) {
+  } else if (diffInSeconds < 604800) {
+    const diffInDays = Math.floor(diffInSeconds / 86400);
     return `${diffInDays}일 전`;
-  } else if (diffInWeeks < 4) {
+  } else if (diffInSeconds < 2592000) {
+    const diffInWeeks = Math.floor(diffInSeconds / 604800);
     return `${diffInWeeks}주 전`;
-  } else if (diffInMonths < 12) {
+  } else if (diffInSeconds < 31536000) {
+    const diffInMonths = Math.floor(diffInSeconds / 2592000);
     return `${diffInMonths}개월 전`;
   } else {
+    const diffInYears = Math.floor(diffInSeconds / 31536000);
     return `${diffInYears}년 전`;
   }
 };
