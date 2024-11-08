04f6b11aff538625dcdcc9a71e48b385d76f7328
Hchan2 <seongchan_@naver.com>
2024-11-08 21:32:19 +0900

    update: 주석 추가 (불필요 시 삭제 예정)

--- Project/twitter-clone-challenge/src/constants.ts
+++ Project/twitter-clone-challenge/src/constants.ts
@@ -1,8 +1,10 @@
+// 회원가입 및 로그인 시, 이메일과 패스워드 정규식표현
 export const emailRegex =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 export const passwordRegex =
   /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
 
+// Tweet 내 촬영 기기 선택 메뉴
 export const SELECT_OPTION_VALUE = [
   "아이폰",
   "갤럭시",
@@ -14,4 +16,5 @@ export const SELECT_OPTION_VALUE = [
   "기타",
 ];
 
+// 이미지 크기 제한
 export const MAX_IMAGE_FILE_SIZE = 2 * 1024 * 1024;
