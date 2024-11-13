1c75f78e45d7414672e8cdb4d3ad98d47162f0b6
Hchan2 <seongchan_@naver.com>
2024-11-13 22:36:29 +0900

    update: 코드 리팩토링

--- Project/twitter-clone-challenge/src/components/screen/MainSlide.tsx
+++ Project/twitter-clone-challenge/src/components/screen/MainSlide.tsx
@@ -1,11 +1,10 @@
 import {
   QueryDocumentSnapshot,
-  QuerySnapshot,
   collection,
   getDocs,
   query,
 } from "firebase/firestore";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { dataBase } from "../../firebase";
 import { ITweet } from "../types/tweet-type";
 import {
@@ -20,92 +19,77 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { useNavigate } from "react-router";
 
+const sliderSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: <></>,
+  nextArrow: <></>,
+  autoplay: true,
+  autoplaySpeed: 5000,
+};
+
+const createTweetData = (doc: QueryDocumentSnapshot): ITweet => {
+  const data = doc.data();
+  return {
+    tweet: data.tweet,
+    createdAt: data.createdAt,
+    userId: data.userId,
+    username: data.username,
+    photo: data.photo,
+    retouch: data.retouch,
+    id: doc.id,
+    likes: data.likes,
+    likedBy: data.likedBy,
+    exclamation: data.exclamation,
+    tags: data.tags,
+    item: data.item,
+    comments: data.comments,
+    location: data.location,
+  };
+};
+
+const fetchTweetsData = async (): Promise<ITweet[]> => {
+  const tweetsQuery = query(collection(dataBase, "tweets"));
+  const tweetsSnapshot = await getDocs(tweetsQuery);
+  return tweetsSnapshot.docs.map(createTweetData);
+};
+
+const getRandomTweets = (tweets: ITweet[], number: number) => {
+  const getRandom = tweets.sort(() => 0.5 - Math.random());
+  return getRandom.slice(0, number);
+};
+
 function MainSlide() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
   const navigate = useNavigate();
-  const settings = {
-    dots: true,
-    infinite: tweets?.length > 1,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: <></>,
-    nextArrow: <></>,
-    autoplay: true,
-    autoplaySpeed: 5000,
-  };
 
-  const mapTweetData = (doc: QueryDocumentSnapshot): ITweet => {
-    const {
-      tweet,
-      createdAt,
-      userId,
-      username,
-      photo,
-      retouch,
-      likes,
-      likedBy,
-      exclamation,
-      tags,
-      item,
-      comments,
-      location,
-    } = doc.data();
-    return {
-      tweet,
-      createdAt,
-      userId,
-      username,
-      photo,
-      retouch,
-      id: doc.id,
-      likes,
-      likedBy,
-      exclamation,
-      tags,
-      item,
-      comments,
-      location,
-    };
-  };
-
-  const fetchTweetsData = async (
-    snapshot: QuerySnapshot
-  ): Promise<ITweet[]> => {
-    const tweets = snapshot.docs.map(mapTweetData);
-
-    return tweets;
-  };
-
-  const getRandomTweets = (tweets: ITweet[], number: number) => {
-    const shuffled = tweets.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, number);
-  };
-
-  const moveDetailPage = (tweet: ITweet) => {
-    const tweetObj = { ...tweet };
-    navigate("/detail", { state: { tweetObj } });
-  };
+  const handleTweetClick = useCallback(
+    (tweetObj: ITweet) => {
+      navigate("/detail", { state: { tweetObj } });
+    },
+    [navigate]
+  );
 
   useEffect(() => {
-    const fetchTweets = async () => {
-      const tweetsQuery = query(collection(dataBase, "tweets"));
-      const tweetsSnapshot = await getDocs(tweetsQuery);
-      const tweets = await fetchTweetsData(tweetsSnapshot);
-      const randomTweets = getRandomTweets(tweets, 10);
+    const fetchAndSetTweets = async () => {
+      const fetchedTweets = await fetchTweetsData();
+      const randomTweets = getRandomTweets(fetchedTweets, 10);
       setTweets(randomTweets);
     };
-    fetchTweets();
+    fetchAndSetTweets();
   }, []);
 
   return (
     <SlideWrapper>
-      <Slider {...settings}>
+      <Slider {...sliderSettings} infinite={tweets.length > 1}>
         {tweets?.map((tweetObj) => (
           <SlideContent
             key={tweetObj.id}
             $backgroundImage={tweetObj?.photo || ""}
-            onClick={() => moveDetailPage(tweetObj)}
+            onClick={() => handleTweetClick(tweetObj)}
           >
             <Overlay />
             <TextContent>
