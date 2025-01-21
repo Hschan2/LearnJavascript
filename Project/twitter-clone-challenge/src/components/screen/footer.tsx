b1797a3bce54669ba16933ecaed0e8c02e5b863d
Hchan2 <seongchan_@naver.com>
2025-01-21 21:14:06 +0900

    update: footer 내용 수정

--- Project/twitter-clone-challenge/src/components/screen/footer.tsx
+++ Project/twitter-clone-challenge/src/components/screen/footer.tsx
@@ -1,7 +1,7 @@
 import { FooterContainer } from "../style/screen-components";
 
 function Footer() {
-  return <FooterContainer>ⓒ 홍성찬</FooterContainer>;
+  return <FooterContainer>ⓒYooP. ALL RIGHTS RESERVED BY 홍성찬</FooterContainer>;
 }
 
 export default Footer;
