41eff5ddee44377e00414661a4e0f79ca0c74b49
Hchan2 <seongchan_@naver.com>
2025-02-03 21:12:21 +0900

    update: 팔로우 화면 스타일 생성

--- Project/twitter-clone-challenge/src/components/style/timeline-components.ts
+++ Project/twitter-clone-challenge/src/components/style/timeline-components.ts
@@ -13,4 +13,36 @@ export const Wrapper = styled.div`
   @media (max-width: 425px) {
     grid-template-columns: 1fr;
   }
+
+  &.followUser-tweets {
+    height: 50%;
+  }
 `;
+
+// Follow Page
+export const FollowWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const UserWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  width: 100%;
+  padding: 28px 14px;
+`;
+
+export const UserAvatar = styled.img`
+  width: 100px;
+  height: 100px;
+  border: 1px solid #f1f5f5;
+  border-radius: 50%;
+`;
+
+export const UserName = styled.span`
+  font-size: 36px;
+`
