e444cfafef38a6a54d1dcf26433755bf4fca3aa2
Hchan2 <seongchan_@naver.com>
2025-08-13 15:56:12 +0900

    update: 직접 입력된 정보, env에서 가져오기

--- Project/twitter-clone-challenge/src/firebase.ts
+++ Project/twitter-clone-challenge/src/firebase.ts
@@ -5,10 +5,10 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "twitter-clone-challenge.firebaseapp.com",
-  projectId: "twitter-clone-challenge",
-  storageBucket: "twitter-clone-challenge.appspot.com",
-  messagingSenderId: "1053362560070",
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
 };
 
