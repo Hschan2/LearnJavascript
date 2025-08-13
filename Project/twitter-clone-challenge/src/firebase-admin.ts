093cf19cbf89d44d06362abb95cfe03f0ed8ca5a
Hchan2 <seongchan_@naver.com>
2025-08-13 21:35:19 +0900

    feat: firebase admin 설정

--- Project/twitter-clone-challenge/src/firebase-admin.ts
+++ Project/twitter-clone-challenge/src/firebase-admin.ts
@@ -5,9 +5,10 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
-    })
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
-export { admin };
+export const adminDb = admin.firestore();
+export const authAdmin = admin.auth();
