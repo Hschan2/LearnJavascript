565683b1bfc07790cbbd15be56d891e4ce5183a9
Hchan2 <seongchan_@naver.com>
2025-08-17 14:36:22 +0900

    update: 버튼 사이즈 조절 (두줄 처리 개선)

--- Project/twitter-clone-challenge/src/features/setting/style/settings-components.ts
+++ Project/twitter-clone-challenge/src/features/setting/style/settings-components.ts
@@ -18,14 +18,15 @@ export const EventButton = styled.button`
   background-color: transparent;
   border: 2px solid ${(props) => props.theme.border};
   border-radius: 14px;
-  width: clamp(120px, 10vw, 150px);
+  width: clamp(140px, 12vw, 180px);
   color: ${(props) => props.theme.text};
   font-size: clamp(12px, 2vw, 14px);
   padding: clamp(8px, 2vw, 10px) clamp(10px, 3vw, 12px);
   cursor: pointer;
 
   svg {
-    width: clamp(30px, 5vw, 50px);
+    width: clamp(20px, 5vw, 40px);
     color: tomato;
+    margin-right: 4px;
   }
 `;
