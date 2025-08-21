df08038f1b509cce0f66dce26e69ac5b7993d946
Hchan2 <seongchan_@naver.com>
2025-08-21 22:18:28 +0900

    update: INITIAL_IMAGE 변수 생성

--- Project/twitter-clone-challenge/src/constants.ts
+++ Project/twitter-clone-challenge/src/constants.ts
@@ -42,3 +42,7 @@ export const SELECT_OPTION_VALUE = [
 
 // 이미지 크기 제한
 export const MAX_IMAGE_FILE_SIZE = 2 * 1024 * 1024;
+
+// 프로필 기본 이미지
+export const INITIAL_IMAGE =
+      "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbxwsqs%2FbtsK1ACfsyI%2FyojjVvJwKpX4bdZ0CtZkJ0%2Fimg.png";
