a2825ad35bc90f651a0229a05c56c9215e4ad241
Hchan2 <seongchan_@naver.com>
2025-09-30 16:24:28 +0900

    update: 알람 메세지 수정

--- Project/twitter-clone-challenge/src/features/notification/components/format-notification-message.tsx
+++ Project/twitter-clone-challenge/src/features/notification/components/format-notification-message.tsx
@@ -3,7 +3,12 @@ import { NotificationType } from "../types/notifications";
 export const formatNotificationMessage = (
   notification: NotificationType
 ): string => {
-  return notification.type === "follow"
-    ? `${notification.senderName}님이 팔로우하였습니다.`
-    : `${notification.senderName}님이 ${notification.tweetTitle}에 좋아요를 눌렀습니다.`;
+  if (notification.type === "follow") {
+    return `${notification.senderName}님이 팔로우하였습니다.`;
+  } else if (notification.type === "like") {
+    return `${notification.senderName}님이 ${notification.tweetTitle}에 좋아요를 눌렀습니다.`;
+  } else if (notification.type === "comment") {
+    return `${notification.senderName}님이 ${notification.tweetTitle}에 댓글을 작성했습니다.`;
+  }
+  return "새로운 알림이 있습니다.";
 };
