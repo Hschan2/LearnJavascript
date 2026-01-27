62f067b7a30a9cc83e73e1109fcafb8ca4e665f8
Hchan2 <seongchan_@naver.com>
2026-01-27 18:44:59 +0900

    refactor: databaseService 함수 적용

--- Project/twitter-clone-challenge/src/features/auth/hooks/saveUserToFirestore.ts
+++ Project/twitter-clone-challenge/src/features/auth/hooks/saveUserToFirestore.ts
@@ -1,19 +1,18 @@
-import { doc, getDoc, setDoc } from "firebase/firestore";
 import { User } from "firebase/auth";
-import { dataBase } from "../../../firebase";
+import { getDocument, setDocument } from "../../../services/databaseService";
 import { INITIAL_IMAGE } from "../../../constants";
 
 export const saveUserToFirestore = async (user: User, name?: string) => {
-  const userRef = doc(dataBase, "signedUsers", user.uid);
-  const userSnap = await getDoc(userRef);
+  const userSnap = await getDocument(["signedUsers", user.uid]);
 
   if (!userSnap.exists()) {
-    await setDoc(userRef, {
+    const newUser = {
       uid: user.uid,
       name: name || user.displayName || "사용자",
       email: user.email,
       avatar: INITIAL_IMAGE,
       createdAt: new Date(),
-    });
+    };
+    await setDocument(["signedUsers", user.uid], newUser);
   }
 };
