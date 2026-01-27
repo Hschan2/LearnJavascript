7d6555996a482a4578a8a6fb6295553b6e482f12
Hchan2 <seongchan_@naver.com>
2026-01-27 18:44:21 +0900

    feat: 데이터베이스 관리하는 함수들 분리하여 생성

---
+++ Project/twitter-clone-challenge/src/services/databaseService.ts
@@ -0,0 +1,66 @@
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  DocumentData,
+} from "firebase/firestore";
+import { dataBase } from "../firebase";
+
+/**
+ * 컬렉션에 새로운 문서 추가 (ID 자동 생성)
+ * @param collectionPath 컬렉션 경로 세그먼트 배열 (예: ["tweets"] 또는 ["tweets", tweetId, "replies"])
+ * @param data 저장할 데이터 객체
+ */
+export const createDocument = async (
+  collectionPath: string[],
+  data: DocumentData
+) => {
+  const collectionRef = collection(
+    dataBase,
+    collectionPath[0],
+    ...collectionPath.slice(1)
+  );
+  return await addDoc(collectionRef, data);
+};
+
+/**
+ * 특정 ID를 가진 문서를 생성하거나 덮어쓰기
+ * @param docPath 문서 경로 세그먼트 배열 (예: ["users", userId])
+ * @param data 저장할 데이터 객체
+ */
+export const setDocument = async (docPath: string[], data: DocumentData) => {
+  const docRef = doc(dataBase, docPath[0], ...docPath.slice(1));
+  return await setDoc(docRef, data);
+};
+
+/**
+ * 기존 문서의 일부 필드 업데이트
+ * @param docPath 문서 경로 세그먼트 배열 (예: ["tweets", tweetId])
+ * @param data 업데이트할 데이터 객체
+ */
+export const updateDocument = async (docPath: string[], data: DocumentData) => {
+  const docRef = doc(dataBase, docPath[0], ...docPath.slice(1));
+  return await updateDoc(docRef, data);
+};
+
+/**
+ * 특정 문서 삭제
+ * @param docPath 문서 경로 세그먼트 배열 (예: ["tweets", tweetId])
+ */
+export const deleteDocument = async (docPath: string[]) => {
+  const docRef = doc(dataBase, docPath[0], ...docPath.slice(1));
+  return await deleteDoc(docRef);
+};
+
+/**
+ * 특정 문서 한 번 가져오기
+ * @param docPath 문서 경로 세그먼트 배열
+ */
+export const getDocument = async (docPath: string[]) => {
+  const docRef = doc(dataBase, docPath[0], ...docPath.slice(1));
+  return await getDoc(docRef);
+};
