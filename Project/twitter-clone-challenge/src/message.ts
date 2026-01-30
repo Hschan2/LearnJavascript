2facecd5d4bbf66a94817f92443ac79f43f27389
Hchan2 <seongchan_@naver.com>
2026-01-30 17:41:11 +0900

    feat: 에러 메세지 변수 생성

---
+++ Project/twitter-clone-challenge/src/message.ts
@@ -0,0 +1,15 @@
+export const API_ERROR_MESSAGE = Object.freeze({
+    NOT_TOKEN_NEW_PASSWORD: "토큰과 새 비밀번호가 필요합니다.",
+    EXPIRED_TOKEN: "토큰이 유효하지 않거나 만료되었습니다",
+    NOT_HAVE_EMAIL: "이메일이 필요합니다.",
+    ALREADY_SIGNED_EMAIL: "이미 가입된 이메일입니다.",
+    FAILED_CODE_EMAIL: "인증 코드 발송 실패",
+    NO_EMAIL_PASSWORD_TOKEN: "이름, 비밀번호, 토큰이 필요합니다.",
+    FALSE_TOKEN: "잘못된 토큰입니다.",
+    FAILED_SIGN: "회원가입 실패",
+    NEED_EMAIL_CODE: "이메일과 코드가 필요합니다.",
+    NOT_CODE: "코드가 없습니다.",
+    EXPIRED_CODE: "코드가 만료되었습니다.",
+    FALSE_CODE: "코드가 틀립니다.",
+    FAILED_CODE_CHECK: "코드 검증 실패"
+})
