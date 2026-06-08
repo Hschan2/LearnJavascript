998352a67924e62aaeb0fff9cd4cf800f06061e9
Hchan2 <seongchan_@naver.com>
2026-06-08 17:16:35 +0900

    Fix: 댓글 목록 UI 내 수정 버튼 및 함수 추가

--- Project/twitter-clone-challenge/src/features/tweet/components/comment-list.tsx
+++ Project/twitter-clone-challenge/src/features/tweet/components/comment-list.tsx
@@ -5,21 +5,27 @@ import {
   CommentContent,
   CommentCreatedTime,
   CommentDeleteButton,
+  CommentEditButton,
   CommentProfile,
   CommentProfileWrapper,
   CommentReplyButton,
   CommentText,
+  EditForm,
+  EditTextArea,
+  EditButtonContainer,
 } from "../styles/tweet-components";
 import { auth } from "../../../firebase";
 import { Avatar } from "../../../layout/styles/screen-components";
 import formattedDate from "../../../shared/hook/formattedDate";
 import LikeBtn from "./like-button";
 import Reply from "./Reply";
+import { SubmitButton } from "../styles/form-components";
 
 interface Props {
   tweetId: string;
   comments: IComment[];
   onDelete: (comment: IComment) => void;
+  onUpdate: (commentId: string, newText: string) => void;
   onCommentLike: (comment: IComment) => void;
 }
 
@@ -27,14 +33,35 @@ const CommentList: FC<Props> = ({
   tweetId,
   comments,
   onDelete,
+  onUpdate,
   onCommentLike,
 }) => {
   const [expandedCommentId, setExpandedCommentId] = useState<string | null>(
     null
   );
+  const [editingCommentId, setEditingCommentId] = useState<string | null>(null);
+  const [editText, setEditText] = useState("");
 
   const toggleReplies = (commentId: string) => {
     setExpandedCommentId((prev) => (prev === commentId ? null : commentId));
+  };
+
+  const handleEditStart = (comment: IComment) => {
+    setEditingCommentId(comment.commentId);
+    setEditText(comment.commentText);
+  };
+
+  const handleEditCancel = () => {
+    setEditingCommentId(null);
+    setEditText("");
+  };
+
+  const handleEditSubmit = async (e: React.FormEvent, commentId: string) => {
+    e.preventDefault();
+    if (!editText.trim()) return;
+    await onUpdate(commentId, editText);
+    setEditingCommentId(null);
+    setEditText("");
   };
 
   return (
@@ -56,30 +83,69 @@ const CommentList: FC<Props> = ({
                 onClick={() => onCommentLike(comment)}
               />
               {comment.commenterId === auth.currentUser?.uid && (
-                <CommentDeleteButton onClick={() => onDelete(comment)}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                    />
-                  </svg>
-                </CommentDeleteButton>
+                <>
+                  <CommentEditButton onClick={() => handleEditStart(comment)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                      />
+                    </svg>
+                  </CommentEditButton>
+                  <CommentDeleteButton onClick={() => onDelete(comment)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </CommentDeleteButton>
+                </>
               )}
             </div>
           </CommentProfileWrapper>
           <CommentContent>
-            <CommentText>{comment.commentText}</CommentText>
-            <CommentCreatedTime>
-              {formattedDate(comment.createdAt)}
-            </CommentCreatedTime>
+            {editingCommentId === comment.commentId ? (
+              <EditForm onSubmit={(e) => handleEditSubmit(e, comment.commentId)}>
+                <EditTextArea
+                  value={editText}
+                  onChange={(e) => setEditText(e.target.value)}
+                  autoFocus
+                />
+                <EditButtonContainer>
+                  <SubmitButton
+                    type="button"
+                    onClick={handleEditCancel}
+                    value="취소"
+                    style={{ backgroundColor: "grey" }}
+                  />
+                  <SubmitButton type="submit" value="수정" />
+                </EditButtonContainer>
+              </EditForm>
+            ) : (
+              <>
+                <CommentText>{comment.commentText}</CommentText>
+                <CommentCreatedTime>
+                  {formattedDate(comment.createdAt)}
+                </CommentCreatedTime>
+              </>
+            )}
             <CommentReplyButton onClick={() => toggleReplies(comment.commentId)}>
               {expandedCommentId === comment.commentId
                 ? "숨기기"
