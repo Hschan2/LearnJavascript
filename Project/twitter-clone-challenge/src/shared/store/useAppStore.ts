2f0970b40e25eb96eff49dcdc225b9e2d1731bb8
Hchan2 <seongchan_@naver.com>
2026-06-18 16:04:25 +0900

    Feat: 프로필 이미지(아바타) 데이터 저장 상태 관리 변수 생성 (캐싱)

--- Project/twitter-clone-challenge/src/shared/store/useAppStore.ts
+++ Project/twitter-clone-challenge/src/shared/store/useAppStore.ts
@@ -5,18 +5,25 @@ interface AppState {
   user: User | null;
   isLoading: boolean;
   darkMode: boolean;
+  profileCache: Record<string, string>;
   setUser: (user: User | null) => void;
   setIsLoading: (isLoading: boolean) => void;
   toggleDarkMode: () => void;
+  setProfileCache: (userId: string, imageUrl: string) => void;
 }
 
 const useAppStore = create<AppState>((set) => ({
   user: null,
   isLoading: true,
   darkMode: false,
+  profileCache: {},
   setUser: (user) => set({ user }),
   setIsLoading: (isLoading) => set({ isLoading }),
   toggleDarkMode: () => set((state) => ({ darkMode: !state.darkMode })),
+  setProfileCache: (userId, imageUrl) =>
+    set((state) => ({
+      profileCache: { ...state.profileCache, [userId]: imageUrl },
+    })),
 }));
 
 export default useAppStore;
