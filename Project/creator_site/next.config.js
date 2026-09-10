869d25c12561af022e4046f2f07e54c5ce6bb024
Hchan2 <seongchan_@naver.com>
2026-09-10 21:36:38 +0900

    fix: API에서 불러온 이미지 도메인 허용

--- Project/creator_site/next.config.js
+++ Project/creator_site/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['raw.githubusercontent.com', 'img1.daumcdn.net'],
+        domains: ['raw.githubusercontent.com', 'img1.daumcdn.net', 'img.youtube.com', 'i.ytimg.com'],
     },
 }
 
