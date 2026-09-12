4301fc1ec673ac90fca1db8d2c3f5c0377a23392
Hchan2 <seongchan_@naver.com>
2026-09-12 20:37:37 +0900

    feat: 상수 값 변수 생성

---
+++ Project/creator_site/app/common/utils/youtube.ts
@@ -0,0 +1,10 @@
+export const YOUTUBE_PLAYLISTS = {
+  portfolio: "PLSIL5GEUInys",
+  motion: "PLYqkGJr-KzGY",
+  travel: "PLXcJqArXkF4s",
+  short: "PLA4HEW0z2fao",
+} as const;
+
+export const YOUTUBE_API_MAX_RESULTS = 50;
+
+export const YOUTUBE_CACHE_TIME = 3600;
